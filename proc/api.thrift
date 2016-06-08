2f345faa7b14341bf7823c15c188ed4fd52c8f97
kmccaffrey <caitiem20@gmail.com>
2016-06-08 16:59:49 -0700

    first take proc api

---
+++ proc/api.thrift
@@ -0,0 +1,40 @@
+service Proc {
+  
+  exception InvalidRequest {
+    1: optional string message
+  }
+
+  exception ScootTooBusy {
+    1: optional i64 retryAfterMs
+  }
+  
+  enum JobType {
+    IRON_TESTS=1,
+    DEFAULT=2,
+  }
+
+  struct Command {
+    1: list<string> cmds
+  }
+
+  struct Task {
+    1: optional string id
+    2: required Command command,
+    3: optional string snapshotId,
+    4: optional string contextId
+  }
+
+  struct Job {
+    1: required string id,
+    2: required list<Task> tasks
+  }
+
+  Job RunJob(
+    1: optional string contextId,
+    2: optional JobType jobType,
+    3: required list<Task> tasks
+  ) throws (
+    1: InvalidRequest ir,
+    2: ScootTooBusy stb
+  )
+}
