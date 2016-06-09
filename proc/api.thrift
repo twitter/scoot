bf833f92e858185f99e3f7089807cd66b5fc1b1c
Caitie McCaffrey <caitiem20@gmail.com>
2016-06-09 12:32:40 -0400

    First take Proc Api (#20)

---
+++ proc/api.thrift
@@ -0,0 +1,38 @@
+service Proc {
+  
+  exception InvalidRequest {
+    1: optional string message
+  }
+
+  exception CanNotScheduleNow {
+    1: optional i64 retryAfterMs
+  }
+  
+  enum JobType {
+    UNKNOWN=1, 
+    IRON_TESTS=2,
+  }
+
+  struct Command {
+    1: list<string> cmds
+  }
+
+  struct Task {
+    1: optional string id
+    2: required Command command,
+    3: optional string snapshotId,
+  }
+
+  struct Job {
+    1: required string id,
+    2: required list<Task> tasks,
+  }
+
+  Job RunJob(
+    1: required list<Task> tasks
+    2: optional JobType jobType,
+  ) throws (
+    1: InvalidRequest ir,
+    2: CanNotScheduleNow cnsn,
+  )
+}
