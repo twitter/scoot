1eccfdfb1a497d726c0fd1fdf6deac93a6c80274
Caitie McCaffrey <caitiem20@gmail.com>
2016-06-09 09:23:23 -0700

    Remove ContextId For Now
    will re-add when we have code that uses it.

--- proc/api.thrift
+++ proc/api.thrift
@@ -21,18 +21,16 @@ service Proc {
     1: optional string id
     2: required Command command,
     3: optional string snapshotId,
-    4: optional string contextId
   }
 
   struct Job {
     1: required string id,
-    2: required list<Task> tasks
+    2: required list<Task> tasks,
   }
 
   Job RunJob(
-    1: optional string contextId,
+    1: required list<Task> tasks
     2: optional JobType jobType,
-    3: required list<Task> tasks
   ) throws (
     1: InvalidRequest ir,
     2: CanNotScheduleNow cnsn,
