391bed1b9417751e8419274fbd053a3eed14f094
Jason Schiller <jschiller@twitter.com>
2016-07-04 12:52:59 -0700

    Added workerserver and workercl

--- deps.sh
+++ deps.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
+# Add new direct and transitive dependencies as submodules.
+# Note: caller must validate/curate/commit changes to .gitmodules and 'vendor/'
 #
-
 set -euo pipefail
 trap "exit" INT TERM
 trap 'rm -rf ${GOPATH}' EXIT
 
 SCOOT_ORIG="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+GOPATH_ORIG="${GOPATH}"
 export GOPATH="$(mktemp -d -t TEMP.XXXXXXX)"
 
-scootdev="${GOPATH}/src/github.com/scootdev"
-mkdir -p "${scootdev}" && cd "${scootdev}"
-git clone https://github.com/scootdev/scoot
-
 get_deps() {
-    cd "$1"
+    cd "${GOPATH}/src/$1"
     for need in $(go list -f '{{join .Deps "\n"}}' ./... | \
                   xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}' 2>&1 | \
                   grep "can't load package" | \
                   sed -E 's,[^"]*"([^"]*).*,\1,' | \
                   grep '\..*/'); do
         go get -t -d "${need}" || true
-        get_deps "${GOPATH}/src/${need}"
+        get_deps "${need}"
     done
 }
 
+if [[ -z "${DEP_REPO:-}" ]]; then
+    DEP_REPO="github.com/scootdev/scoot"
+    scootdev="${GOPATH}/src/$(dirname ${DEP_REPO})"
+    mkdir -p "${scootdev}" && cd "${scootdev}"
+    git clone "https://${DEP_REPO}"
+else
+    depdir="${GOPATH}/src/$(dirname ${DEP_REPO})"
+    mkdir -p "${depdir}" && cd "${depdir}"
+    cp -r "${GOPATH_ORIG}/src/${DEP_REPO}" ./
+fi
+
 echo "Darwin Deps."
 export GOOS=darwin GOARCH=amd64
-get_deps "${scootdev}/scoot"
+get_deps "${DEP_REPO}"
 
 echo "Windows Deps."
 export GOOS=windows GOARCH=amd64
-get_deps "${scootdev}/scoot"
+get_deps "${DEP_REPO}"
 
 HANDLED=$(find ${GOPATH} -name .git | sort | uniq | sed -E "s,${GOPATH}/src/|/\.git,,g" | grep -v scootdev)
 
@@ -39,6 +48,6 @@ for dep in ${HANDLED}; do
     url=$(cd "${GOPATH}/src/${dep}" && git config --get remote.origin.url)
     sha1=$(cd "${GOPATH}/src/${dep}" && git rev-parse HEAD)
     grep "${dep}" ".gitmodules" &>/dev/null && continue || echo "Adding ${dep}"
-    git submodule "${url}" "vendor/${dep}"
-    git config .gitmodules "submodule.vendor/${dep}.branch" "${sha1}"
+    git submodule add "${url}" "vendor/${dep}"
+    git config -f .gitmodules "submodule.vendor/${dep}.branch" "${sha1}"
 done
---
+++ workerapi/client/commands.go
@@ -0,0 +1,115 @@
+package client
+
+import (
+	"log"
+	"strings"
+
+	"github.com/luci/go-render/render"
+	"github.com/scootdev/scoot/workerapi/gen-go/worker"
+	"github.com/spf13/cobra"
+)
+
+// Run
+var runArgv *string
+var runcmd *worker.RunCommand = worker.NewRunCommand()
+
+func makeRunCmd(c *Client) *cobra.Command {
+	r := &cobra.Command{
+		Use:   "run",
+		Short: "runs a command",
+		RunE:  c.run,
+	}
+	r.Flags().StringVar(&c.addr, "addr", "localhost:9090", "address to connect to")
+	runArgv = r.Flags().String("argv", "", "comma separated list of binary and args")
+	runcmd.SnapshotId = r.Flags().String("snapshotid", "", "snapshot/patch id.")
+	runcmd.TimeoutMs = r.Flags().Int32("timeout_ms", 0, "timeout before aborting cmd")
+	return r
+}
+func (c *Client) run(cmd *cobra.Command, args []string) error {
+	runcmd.Argv = strings.Split(*runArgv, ",")
+	log.Println("Calling run rpc to cloud worker", args, render.Render(runcmd))
+
+	client, err := c.Dial()
+	if err != nil {
+		return err
+	}
+
+	status, err := client.Run(runcmd)
+	log.Println(render.Render(status))
+	return err
+}
+
+// Query
+var queryRunId *string
+
+func makeQueryCmd(c *Client) *cobra.Command {
+	r := &cobra.Command{
+		Use:   "query",
+		Short: "queries a runId",
+		RunE:  c.query,
+	}
+	r.Flags().StringVar(&c.addr, "addr", "localhost:9090", "address to connect to")
+	queryRunId = r.Flags().String("id", "", "status of a run.")
+	return r
+}
+func (c *Client) query(cmd *cobra.Command, args []string) error {
+	log.Println("Calling query rpc to cloud worker", args)
+
+	client, err := c.Dial()
+	if err != nil {
+		return err
+	}
+
+	status, err := client.Query(*queryRunId)
+	log.Println(render.Render(status))
+	return err
+}
+
+// Abort
+var abortRunId *string
+
+func makeAbortCmd(c *Client) *cobra.Command {
+	r := &cobra.Command{
+		Use:   "abort",
+		Short: "aborts a runId",
+		RunE:  c.abort,
+	}
+	r.Flags().StringVar(&c.addr, "addr", "localhost:9090", "address to connect to")
+	abortRunId = r.Flags().String("id", "", "status of a run.")
+	return r
+}
+func (c *Client) abort(cmd *cobra.Command, args []string) error {
+	log.Println("Calling abort rpc to cloud worker", args)
+
+	client, err := c.Dial()
+	if err != nil {
+		return err
+	}
+
+	status, err := client.Abort(*abortRunId)
+	log.Println(render.Render(status))
+	return err
+}
+
+// QueryWorker
+func makeQueryworkerCmd(c *Client) *cobra.Command {
+	r := &cobra.Command{
+		Use:   "queryworker",
+		Short: "queries worker status",
+		RunE:  c.queryworker,
+	}
+	r.Flags().StringVar(&c.addr, "addr", "localhost:9090", "address to connect to")
+	return r
+}
+func (c *Client) queryworker(cmd *cobra.Command, args []string) error {
+	log.Println("Calling queryworker rpc to cloud worker", args)
+
+	client, err := c.Dial()
+	if err != nil {
+		return err
+	}
+
+	status, err := client.QueryWorker()
+	log.Println(render.Render(status))
+	return err
+}
