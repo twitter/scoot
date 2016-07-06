a64f021a1242bc6f9760a05adac3fc3379f72a43
Caitie McCaffrey <caitiem20@gmail.com>
2016-07-06 16:52:51 -0700

    Caitie/dynamic cluster distributor (#47)
    Pool Distributor Supports a Dynamic Cluster
    
    * Adding a Dynamic Cluster Interface that extends Cluster Interface. New DynamicCluster methods are expected to return a ClusterState object which provides initial nodes and a channel for updates.  This simplifies distributor logic, and avoids rebuilding the state of the cluster on every request, updated dynamicCluster implementation to support these new features.  
    * Made LocalNode accessible outside the package
    * Updated Demo to Use DynamicCluster, and remove/add nodes. The current dummy function is not all that random, it can definitely be improved
    * PoolDistributor updates if initialized with a DynamicCluster.  Maintains a toRemove list.  On Retrieve or Release, ensures nodes on the toRemove list are not returned or re-added to the free list.  Once a node has been discarded its removed from the toRemove List.

--- sched/demo/main.go
+++ sched/demo/main.go
@@ -9,22 +9,32 @@ import (
 	cm "github.com/scootdev/scoot/sched/clustermembership"
 	distributor "github.com/scootdev/scoot/sched/distributor"
 
+	"math/rand"
 	"os"
+	"runtime"
 	"sync"
+	"time"
 )
 
 /* demo code */
 func main() {
 
-	cluster := ci.StaticLocalNodeClusterFactory(10)
+	runtime.GOMAXPROCS(2)
+
+	cluster, clusterState := ci.DynamicLocalNodeClusterFactory(10)
 	fmt.Println("clusterMembers:", cluster.Members())
 	fmt.Println("")
 
 	workCh := make(chan sched.Job)
-	distributor := distributor.NewPoolDistributor(cluster)
+	distributor := distributor.NewDynamicPoolDistributor(clusterState)
 	saga := s.MakeInMemorySaga()
 
+	go func() {
+		generateClusterChurn(cluster, clusterState)
+	}()
+
 	var wg sync.WaitGroup
+
 	wg.Add(2)
 
 	go func() {
@@ -126,4 +136,45 @@ func generateTasks(work chan<- sched.Job, numTasks int) {
 		}
 	}
 	close(work)
+}
+
+func generateClusterChurn(cluster cm.DynamicCluster, clusterState cm.DynamicClusterState) {
+
+	//TODO: Make node removal more random, pick random index to remove instead
+	// of always removing from end
+
+	totalNodes := len(clusterState.InitialMembers)
+	addedNodes := clusterState.InitialMembers
+	removedNodes := make([]cm.Node, 0, len(addedNodes))
+
+	for {
+		// add a node
+		if rand.Intn(2) != 0 {
+			if len(removedNodes) > 0 {
+				var n cm.Node
+				n, removedNodes = removedNodes[len(removedNodes)-1], removedNodes[:len(removedNodes)-1]
+				addedNodes = append(addedNodes, n)
+				cluster.AddNode(n)
+				fmt.Println("ADDED NODE: ", n.Id())
+			} else {
+				n := ci.LocalNode{
+					Name: fmt.Sprintf("dynamic_node_%d", totalNodes),
+				}
+				totalNodes++
+				addedNodes = append(addedNodes, n)
+				cluster.AddNode(n)
+				fmt.Println("ADDED NODE: ", n.Id())
+			}
+		} else {
+			if len(addedNodes) > 0 {
+				var n cm.Node
+				n, addedNodes = addedNodes[len(addedNodes)-1], addedNodes[:len(addedNodes)-1]
+				removedNodes = append(removedNodes, n)
+				cluster.RemoveNode(n.Id())
+				fmt.Println("REMOVED NODE: ", n.Id())
+			}
+		}
+
+		time.Sleep(100 * time.Millisecond)
+	}
 }
