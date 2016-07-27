e8ec2826d3b60ed407449e5f0725842c70c0c800
Caitie McCaffrey <caitiem20@gmail.com>
2016-07-27 09:08:26 -0700

    Combinte Scheduler and ApiServer into one binary (#65)
    Combined Scheduler & API Server into one binary.  
    Tested by running scootapi command line tool against combined server locally. 
    We still need to rename this ... open to suggestions

--- binaries/scheduler/main.go
+++ binaries/scheduler/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"github.com/apache/thrift/lib/go/thrift"
 	clusterimpl "github.com/scootdev/scoot/cloud/cluster/memory"
 	"github.com/scootdev/scoot/saga"
 	"github.com/scootdev/scoot/sched/distributor"
 	queueimpl "github.com/scootdev/scoot/sched/queue/memory"
 	"github.com/scootdev/scoot/sched/scheduler"
 	"github.com/scootdev/scoot/sched/worker/fake"
+	"github.com/scootdev/scoot/scootapi/server"
 	"log"
 	"sync"
 )
 
 func main() {
-	fmt.Println("Starting Scheduler")
+	log.Println("Starting Cloud Scoot API Server & Scheduler")
+	addr := "localhost:9090"
+
+	protocolFactory := thrift.NewTBinaryProtocolFactoryDefault()
+	transportFactory := thrift.NewTTransportFactory()
 
 	// Create Cluster
 	// TODO: replace with actual cluster implementation, currently dummy in memory cluster
@@ -30,11 +35,24 @@ func main() {
 	// TODO: Replace with Durable WorkQueue, currently in Memory Only
 	workQueue := queueimpl.NewSimpleQueue(1000)
 
+	handler := server.NewHandler(workQueue, sagaCoordinator)
+
 	var wg sync.WaitGroup
-	wg.Add(1)
+	wg.Add(2)
+
+	// Start API Server
+	go func() {
+		log.Println("Starting API Server")
+		defer wg.Done()
+		err := server.Serve(handler, addr, transportFactory, protocolFactory)
+		if err != nil {
+			log.Fatal("Error serving Scoot API: ", err)
+		}
+	}()
 
 	// Go Routine which takes data from work queue and schedules it
 	go func() {
+		log.Println("Starting Scheduler")
 		defer wg.Done()
 		scheduler.GenerateWork(sched, workQueue.Chan())
 	}()
