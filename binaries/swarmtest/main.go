a70d365217cf6af7f6f603abd5dcbaa1869bf794
Caitie McCaffrey <caitiem20@gmail.com>
2016-08-24 13:18:04 -0700

    Swarmtest refactor (#82)
    * refactoring swarmtest so it can run as an integration test.  It currently is very flaky as a unit test as part of travisCI filed a bug to investigate.  
    * added a mutex to swarmtest to avoid race conditions, also had to remove cmd.Wait()  due to complicated race conditions.  Not sure how to have a cmd.Wait() in one go routine followed by a cmd.Kill() in another one and not trigger the race detector without doing a big refactor...
    * added swarmtest binary to binaries folders so it can be ran locally as well
    * moved job & task generators to tests folder
    * added numJobs and timeout to run_smoketest_cmd

---
+++ binaries/swarmtest/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	swarmtest "github.com/scootdev/scoot/tests/swarmtest"
+	"time"
+)
+
+// Runs an end to end integration test with work being scheduled
+// via the ScootApi placed on the WorkQueue, Dequeued by the Scheduler
+// and ran on local instances of Workers.
+func main() {
+	s := swarmtest.SwarmTest{}
+
+	err := s.InitOptions(map[string]interface{}{
+		"num_workers": 20,
+		"num_jobs":    100,
+		"timeout":     100 * time.Second,
+	})
+	if err != nil {
+		panic(err)
+	}
+	s.Main()
+}
