1f2a33bbcb346241039308157b69785085b46079
rcouto <rcouto@twitter.com>
2016-09-16 13:12:22 -0400

    dont use args for test target command's argv

--- scootapi/client/test_targets_cmd.go
+++ scootapi/client/test_targets_cmd.go
@@ -16,12 +16,13 @@ func makeTestTargetsCmd(c *Client) *cobra.Command {
 	r.Flags().StringVar(&c.addr, "addr", "localhost:9090", "address to connect to")
 	r.Flags().StringVar(&snapshotID, "snapshot_id", scoot.TaskDefinition_SnapshotId_DEFAULT, "snapshot ID to run job against")
 	r.RunE = func(cmd *cobra.Command, args []string) error {
-		return c.testTargets(cmd, args, snapshotID)
+		log.Println(args)
+		return c.testTargets(cmd, snapshotID)
 	}
 	return r
 }
 
-func (c *Client) testTargets(cmd *cobra.Command, args []string, snapshotID string) error {
+func (c *Client) testTargets(cmd *cobra.Command, snapshotID string) error {
 
 	client, err := c.Dial()
 	if err != nil {
