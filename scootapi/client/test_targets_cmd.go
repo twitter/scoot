9ef66f79bd0389bd891e1783ae130ad8e413f9b8
rcouto <rcouto@twitter.com>
2016-09-16 11:48:23 -0400

    debug

--- scootapi/client/test_targets_cmd.go
+++ scootapi/client/test_targets_cmd.go
@@ -30,7 +30,7 @@ func (c *Client) testTargets(cmd *cobra.Command, args []string, snapshotID strin
 	task := scoot.NewTaskDefinition()
 	task.Command = scoot.NewCommand()
 	task.Command.Argv = args
-	task.Command.Argv = append(task.Command.Argv, "./pants test "+getTargets()+":")
+	task.Command.Argv = append(task.Command.Argv, "./pants test util/util-function/src/main/java:")
 	// for _, t := range getTargets() {
 	// 	task.Command.Argv = append(task.Command.Argv, "./pants test "+t+":")
 	// }
