90045c2a79f3675a620b59f2132ef0bd8c003aaf
kmccaffrey <caitiem20@gmail.com>
2016-09-23 14:45:51 -0700

    config file part of binary

--- binaries/scheduler/main.go
+++ binaries/scheduler/main.go
@@ -1,16 +1,22 @@
 package main
 
+//go:generate go-bindata -pkg "config" -o ./config/config.go config
+
 import (
+	"github.com/scootdev/scoot/binaries/scheduler/config"
 	"github.com/scootdev/scoot/scootapi/server"
+	"log"
 )
 
 func main() {
-	config := `{
-		"Cluster": {
-			"Type": "memory",
-			"Count": 10
-		}
-	}`
+
+	configFileName := "config/config.json"
+	config, err := config.Asset(configFileName)
+
+	if err != nil {
+		log.Fatalf("Error Loading Config File: %v, with Error: %v", configFileName, err)
+	}
+
 	bag, schema := server.Defaults()
 	server.RunServer(bag, schema, []byte(config))
 }
