8bddbbb9fd445d78aec6d6c70947a857bec092fd
kmccaffrey <caitiem20@gmail.com>
2016-10-17 17:34:49 -0700

    Add Config Flag and parsing for when we want to use config

--- binaries/workerserver/main.go
+++ binaries/workerserver/main.go
@@ -1,21 +1,31 @@
 package main
 
-// go:generate go-bindata -pkg "config" -o ./config/config.go config
+//go:generate go-bindata -pkg "config" -o ./config/config.go config
 
 import (
 	"flag"
+	"log"
+
 	"github.com/apache/thrift/lib/go/thrift"
+	"github.com/scootdev/scoot/binaries/workerserver/config"
 	"github.com/scootdev/scoot/common/endpoints"
 	"github.com/scootdev/scoot/common/stats"
+	"github.com/scootdev/scoot/config/jsonconfig"
 	"github.com/scootdev/scoot/os/temp"
 	"github.com/scootdev/scoot/workerapi/server"
 )
 
 var thriftAddr = flag.String("thrift_addr", "localhost:9090", "port to serve thrift on")
 var httpAddr = flag.String("http_addr", "localhost:9091", "port to serve http on")
+var configFlag = flag.String("config", "local.local", "Worker Server Config (either a filename like local.local or JSON text")
 
 func main() {
 	flag.Parse()
+
+	configText, err := jsonconfig.GetConfigText(*configFlag, config.Asset)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	bag, schema := server.Defaults()
 	bag.PutMany(
@@ -25,5 +35,6 @@ func main() {
 		},
 		func() (*temp.TempDir, error) { return temp.TempDirDefault() },
 	)
-	server.RunServer(bag, schema, nil)
+
+	server.RunServer(bag, schema, configText)
 }
