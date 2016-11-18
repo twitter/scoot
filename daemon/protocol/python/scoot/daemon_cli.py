cdc2cfaef6484af78f2e7bb38dab9c73d16b546f
jeanette bruno <jbruno@twitter.com>
2016-11-18 10:57:00 -0500

    Scoot175 - daemon command line is working

--- daemon/protocol/python/scoot/daemon_cli.py
+++ daemon/protocol/python/scoot/daemon_cli.py
@@ -1,9 +1,12 @@
+#This is the command line entry to the scoot daemon.
+#This code uses docopt to parse the command line.  docopt uses the  __doc__ string below 
+#for both parsing the command line into a dict and for displaying help.
 """
 Usage:
   daemon_cli.py snapshot create <srcDir>
   daemon_cli.py snapshot checkout <snapshotId> <destDir>
-  daemon_cli.py exec run <command> [--snapshotId=<sid>] [--timeout=<ns>]
-  daemon_cli.py exec poll <runIds> [--wait=<wait>] [--all]
+  daemon_cli.py exec run <command> ... --snapshotId=<sid> [--timeout=<ns>]
+  daemon_cli.py exec poll <runId> ... [--wait=<wait>] [--all]
   daemon_cli.py exec abort <runId>
   daemon_cli.py echo <ping>
 
@@ -19,7 +22,7 @@ checkout Copies the contents of snapshotId to destDir.  Files from snapshotId wi
 
 execution commands:
 run             Copies the files from snapshotId (if a snapshotId is supplied), and runs the command.  
-                if timeout is supplied, the daemon will wait timeout seconds for the command to complete.  
+                if timeout_ns is supplied, the daemon will wait timeout (seconds) for the command to complete.  
                 Default timeout is 0.5 seconds.
 
 poll            Get the status of runs.  RunIds must be a comma delimited list of run ids (1,3,5,6).
@@ -35,122 +38,153 @@ echo            The Scoot daemon echo's <ping> back to the client.  Use this com
                 is running.
 
 Options:
-  -h --help          Show this screen.
-  --snapshotId=<sid>  Snapshot to install before running the command.
-  --timeout=<ns>     Maximum time(ms) to allow the command to run. Default 500ms
-  --wait=<wait>      <0:wait indefinitely for at least one run to complete. 
-                     0: return immediately with the status(s).  
-                     >0: wait up to this time(seconds) for at least one of the runs to finish.  
-                     Default: 0
-  --all              Return the status of all the runs not just finished runs.
+  -h --help               Show this screen.
+  --snapshotId=<sid>      Snapshot to install before running the command.
+  --timeout=<seconds>     Maximum time(ms) to allow the command to run. Default 500ms
+  --wait=<wait>           <0:wait indefinitely for at least one run to complete. 
+                          0: return immediately with the status(s).  
+                          >0: wait up to this time(seconds) for at least one of the runs to finish.  
+                          Default: 0
+  --all                   Return the status of all the runs not just finished runs.
 
 """
 import docopt
 import os
 import sys
 import re
-from scoot.docopt import DocoptExit
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../protocol'))
 import client_lib as proto
 
 def display_statuses(statuses):
-    for status in statuses:
-        print("\n\{RunId:{0},\n State:{1},\n ExitCode:{2},\n {Error:{3},\n Snapshot:{5}\n\}".format(status.runId, proto.ScootStatus.State(status.state), status.ExitCode, status.Error, status.Snapshot))
-
+  for status in statuses:
+    print("\nRunId:{0},\n\tState:{1},\n\tExitCode:{2},\n\tError:{3},\n\tSnapshot:{4}\n".format(status.run_id, proto.display_state(status.state), status.exit_code, status.error, status.snapshot_id))
 
 if __name__ == '__main__':
+  # parse the command line
+  try:
+    cmd = docopt.docopt(__doc__)
+  except Exception as e:
+    raise docopt.DocoptExit("error parsing command: '{0}'.".format(str(e)))
+  
+  # make the client connection
+  try:
+    proto.start()
+  #handle errors making the client connection
+  except proto.ScootException as e:
+    if re.search("UNAVAILABLE", e) > 0:
+      raise docopt.DocoptExit("Scoot Daemon is not running!\n")
+    raise docopt.DocoptExit("connecting to daemon error: '{0}' (make sure you have started the daemon).".format(str(e)))       
+        
+        
+  # snapshot create:
+  if cmd['create']:
+    #verify args
+    if not cmd['<srcDir>']:
+      raise docopt.DocoptExit("Must provide srcDir!")
+    
+    #run the command
     try:
-        cmd = docopt.docopt(__doc__)
-    except Exception as e:
-        print("exception:{0}".format(e))
-        raise docopt.DocoptExit(e)
-    try:
-#         print("starting client")
-        proto.start()
+      sid = proto.create_snapshot(cmd['<srcDir>'])
+      # print the output
+      print("snapshot id = {0}".format(sid))
+    #handle errors running the command
     except proto.ScootException as e:
-        if re.search("UNAVAILABLE", e) > 0:
-            raise docopt.DocoptExit("Scoot Daemon is not running!\n")
-        raise docopt.DocoptExit(e)          
+      if re.search("Not started", e) > 0 or re.search("UNAVAILABLE", e) > 0:
+        raise docopt.DocoptExit("Scoot Daemon is not running!\n")
+      raise docopt.DocoptExit("create snapshot error: '{0}'.".format(str(e)))       
         
-    if cmd['snapshot'] and cmd['create']:
-        if not cmd['<srcDir>']:
-            raise docopt.DocoptExit("Must provide srcDir!")
-        try:
-            sid = proto.create_snapshot(cmd['<srcDir>'])
-#             print("createSnapshot({0})".format(cmd['<srcDir>']))
-            print("snapshot id = {0}".format(sid))
-        except proto.ScootException as e:
-            if re.search("Not started", e) > 0 or re.search("UNAVAILABLE", e) > 0:
-                raise docopt.DocoptExit("Scoot Daemon is not running!\n")
-            raise docopt.DocoptExit(e)          
         
-    elif cmd['checkout']:
-        if not cmd['<snapshotId>']:
-            raise docopt.DocoptExit("Must supply a snapshot id.")
-        if not cmd["<destDir>"]:
-            raise docopt.DocoptExit("Must supply a destination directory.")
-        try:
-            r = proto.checkout_snapshot(snapshot_id=cmd['<snapshotId>'], dirpath=cmd["<destDir>"])
-#             print("checkout({0}, {1})".format(cmd['<snapshotId>'], dirpath=cmd["<destDir>"]))
-        except Exception as e:
-            raise docopt.DocoptExit(e)
+  # snapshot checkout:
+  elif cmd['checkout']:
+    #verify args
+    if not cmd['<snapshotId>']:
+      raise docopt.DocoptExit("Must supply a snapshot id.")
+    if not cmd["<destDir>"]:
+      raise docopt.DocoptExit("Must supply a destination directory.")
+    #run the command
+    try:
+      r = proto.checkout_snapshot(snapshot_id=cmd['<snapshotId>'], dirpath=cmd["<destDir>"])
+    #handle errors running the command
+    except Exception as e:
+      raise docopt.DocoptExit("snapshot checkout error: '{0}'.".format(str(e)))
         
-    elif cmd['run']:
-        timeout = cmd['--timeout']
-        if not timeout:
-            timeout = 500000
-        else:
-            try:
-                timeout = int(timeout)
-            except Exception as e:
-                raise docopt.DocoptExit("invalid value for timeout, must be an integer.")
-        if not cmd["--snapshotId"]:
-            raise docopt.DocoptExit("SnapshotId is missing")
-        args = re.split(" ", cmd['<command>'])  #TODO - fix this - need to handle spaces in \"
+        
+  # run:
+  elif cmd['run']:
+    #verfiy args
+    timeout = cmd['--timeout']
+    if not timeout:
+      timeout_ns = int(1e9)
+    else:
+      try:
+        timeout_ns = int(int(timeout) * 1e9)
+      except Exception as e:
+        raise docopt.DocoptExit("invalid value for timeout, must be an integer or decimal number.")
+    snapshotId = cmd['--snapshotId']
+    args = cmd['<command>']
 
-        try:
-            runId = proto.run(snapshot_id=cmd["--snapshotId"], argv=args, timeout_ns=timeout)
-#             print("run({0}, {1}, {2})".format(["--snapshotId"], cmd["<command>"], timeout))
-            print("run id = {0}".format(runId))
-        except proto.ScootException as e:
-            if re.search("No resources available", e) is None:
-                print(e)
-            elif re.search("Not started", e) > 0 or re.search("UNAVAILABLE", e) > 0:
-                raise docopt.DocoptExit("Scoot Daemon is not running!\n")
-            raise docopt.DocoptExit(e)
-        except Exception as e1:
-            raise docopt.DocoptExit(e1)
-        
-    elif cmd['poll']:
-        wait = cmd["--wait"]
-        if not wait:
-            wait = 0
-        runIds = cmd["<runIds>"]
-        if not cmd["<runIds>"]:
-            raise docopt.DocoptExit("RunId list is empty!")
-        
-        try:
-            statuses = proto.poll(runIds=cmd["<runIds>"], timeout_ns=wait, return_all=cmd['--all'])
-#             print("poll({0}, {1}, {2}".format(cmd["<runIds>"], wait, ['--all']))
-            display_statuses(statuses)
-        except proto.ScootException as e:
-            if re.search("Not started", e) > 0 or re.search("UNAVAILABLE", e) > 0:
-                raise docopt.DocoptExit("Scoot Daemon is not running!\n")
-            raise docopt.DocoptExit(e)
-        
-    elif cmd['abort']:
-        raise docopt.DocoptExit("Abort not implemented yet.")
-        
-    elif cmd['echo']:
-        if not cmd['<ping>']:
-            raise docopt.DocoptExit("Must supply <ping> value to echo.")
-        try:
-            echo = proto.echo(ping=cmd['<ping>'])
-#             print("echo({0})".format(cmd['<ping>']))
-            print("{0}".format(echo)) 
-        except proto.ScootException as e:
-            if re.search("Not started", str(e)) > 0 or re.search("UNAVAILABLE", str(e)) > 0:
-                raise docopt.DocoptExit("Scoot Daemon is not running!\n")
-            raise docopt.DocoptExit(e)
-             
+    #run the command
+    try:
+      runId = proto.run(snapshot_id=snapshotId, argv=args, timeout_ns=timeout_ns)
+      #print the output
+      print("run id = {0}".format(runId))
+    #handle errors running the command
+    except proto.ScootException as e:
+      if re.search("No resources available", e) is None:
+        print(e)
+      elif re.search("Not started", e) > 0 or re.search("UNAVAILABLE", e) > 0:
+        raise docopt.DocoptExit("Scoot Daemon is not running!\n")
+      else:
+        raise docopt.DocoptExit("run request error: '{0}'".format(str(e)))
+    except Exception as e1:
+      raise docopt.DocoptExit("run request error:'{0}'".format(str(e1)))
+      
+      
+  #poll:
+  elif cmd['poll']:
+    #verify args
+    wait = cmd["--wait"]
+    if not wait:
+      wait = 0
+    try:
+      wait = int(wait)
+    except Exception as e:
+      raise docopt.DocoptExit("Wait must be an integer.")
+    runIds = cmd["<runId>"]
+    if not cmd["<runId>"]:
+      raise docopt.DocoptExit("RunId list is empty!")
+    
+    #run the command
+    try:
+      statuses = proto.poll(run_ids=cmd["<runId>"], timeout_ns=wait, return_all=cmd['--all'])
+      # print the output
+      display_statuses(statuses)
+    #handle errors running the command
+    except proto.ScootException as e:
+      if re.search("Not started", e) > 0 or re.search("UNAVAILABLE", e) > 0:
+        raise docopt.DocoptExit("Scoot Daemon is not running!\n")
+      raise docopt.DocoptExit("poll request error:'{0}'.".format(str(e)))
+      
+      
+  #abort:
+  elif cmd['abort']:
+    raise docopt.DocoptExit("Abort not implemented yet.")
+      
+      
+  #echo:
+  elif cmd['echo']:
+    #verify args
+    if not cmd['<ping>']:
+      raise docopt.DocoptExit("Must supply <ping> value to echo.")
+    
+    #run the command
+    try:
+      echo = proto.echo(ping=cmd['<ping>'])
+      #print the output
+      print("{0}".format(echo)) 
+    #handle errors running the command
+    except proto.ScootException as e:
+      if re.search("Not started", str(e)) > 0 or re.search("UNAVAILABLE", str(e)) > 0:
+        raise docopt.DocoptExit("Scoot Daemon is not running!\n")
+      raise docopt.DocoptExit("echo request error:'{0}'".format(str(e)))
--- daemon/protocol/python/tests/daemon_cli_test.py
+++ daemon/protocol/python/tests/daemon_cli_test.py
@@ -7,11 +7,11 @@ import subprocess
 import tempfile
 import time
 import unittest
-from Carbon.Aliases import false
+from matplotlib.compat.subprocess import CalledProcessError
 
 gopath = os.environ['GOPATH']
 pypath = gopath + '/src/github.com/scootdev/scoot/daemon/protocol/python/scoot'
-cliRef = ['python', pypath+'/daemon_cli.py']
+cliRef = ['python', pypath + '/daemon_cli.py']
 createS = ['snapshot', 'create']
 checkoutS = ['snapshot', 'checkout']
 runE = ['exec', 'run']
@@ -20,101 +20,139 @@ pollE = ['exec', 'poll']
 echo = ['echo']
 
 class TestCliCommands(unittest.TestCase):
-    daemonProcess = None
-    pypath = ''
-      
-    def setUp(self):
-        self.tmpdir = tempfile.mkdtemp()
-
-        # Note: the following does not work - the process from the pool does not have GOPATH defined so it can't find the binary
-        try:
-            self.daemonProcess = subprocess.Popen(['{0}/bin/daemon'.format(gopath), '-execer_type', 'os'])
-        except Exception as e:
-            self.fail('Fail:{0}'.format(e))  
-        started = false
-        start = time.time()
-        elapsedTime = 0
-        cmd = cliRef + echo + ['ping']
-        while not started and elapsedTime < 4.0:
-            try:    
-                r = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-                if self.verifyOut(r, 'ping', False):
-                    started = True
-            except Exception as e:
-                elapsedTime = time.time() - start
-                time.sleep(0.25)
-        
-        if not started:
-            self.daemonProcess.kill()
-            self.fail("Daemon didn't start within '{0}'".format(elapsedTime))
-
-    def tearDown(self):
-        self.daemonProcess.kill()
-        shutil.rmtree(self.tmpdir)
-        unittest.TestCase.tearDown(self)
-
-    def verifyOut(self, out, expected, failImmediately):
-        m = re.search(expected, out)
-        if failImmediately:
-            self.assertTrue(m != None, "expected to find {0} in '{1}'".format(expected, out))
-            
-        return m != None
-
-    def test_happyPath(self):
-        # issue createSnapshot validate that get a snapShot id 
-        cmd = cliRef + createS + [self.tmpdir]
-        r = subprocess.check_output(cmd)
-        self.verifyOut(r, 'snapshot id = [0-9]+', True)
-        m = re.findall(r'[0-9]+', r)
-        sId = m[0]
-        
-        # issue createSnapshot without a path validate get error message and usage prompt 
-        cmd = cliRef + checkoutS + [sId, self.tmpdir]
-        r = subprocess.check_output(cmd).rstrip()
-        self.assertTrue(r == '', "Error: expected '' from checkout, got '{0}'".format(r))
-
-        # run with snapshot id and timeout        
-        cmd = cliRef + runE + ['ls', '--snapshotId', sId, '--timeout', '2000000']
-        r = subprocess.check_output(cmd).rstrip()
-        self.verifyOut(r, 'run id = [0-9]+', True)
-        r1 = self.get_run_id(r)
+  daemonProcess = None
+  pypath = ''
      
-        # run with snapshot id         
-        cmd = cliRef + runE + ['\"ls\"', '--snapshotId', sId]
-        r = subprocess.check_output(cmd).rstrip()
-        self.verifyOut(r, 'run id = [0-9]+', True)
-        r2 = self.get_run_id(r)
-        
-        pollIds = [r1, r2]
-        
-        # TODO implement poll check
+  def setUp(self):
+    self.tmpdir = tempfile.mkdtemp()
 
-    
-    def get_run_id(self, out):   
-        m = re.findall(r'[0-9]+', out)
-        return m[0]
-        
-        
-    def assert_invalid_command(self, cmd):
-        try:
-            r = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
-        except Exception as e:
-            return
-        self.fail('{0} did not return an error.'.format(*cmd))
+    # Note: the following does not work - the process from the pool does not have GOPATH defined so it can't find the binary
+    try:
+      self.daemonProcess = subprocess.Popen(['{0}/bin/daemon'.format(gopath), '-execer_type', 'os'])
+    except Exception as e:
+      self.fail('Fail:{0}'.format(e))  
+    started = False
+    start = time.time()
+    elapsedTime = 0
+    cmd = cliRef + echo + ['ping']
+    while not started and elapsedTime < 4.0:
+      try:    
+        r = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        if self.verifyOut(r, 'ping', False):
+          started = True
+      except Exception as e:
+        elapsedTime = time.time() - start
+        time.sleep(0.25)
+       
+    if not started:
+      self.daemonProcess.kill()
+      self.fail("Daemon didn't start within '{0}'".format(elapsedTime))
 
-         
-    def test_cmdErrors(self):
-        # issue createSnapshot without a path validate get error message and usage prompt 
-        cmd = cliRef + createS
-        self.assert_invalid_command(cmd)
-         
-        cmd = cliRef + checkoutS
-        self.assert_invalid_command(cmd)
- 
-        cmd = cliRef + checkoutS + ['34']
-        self.assert_invalid_command(cmd)
+  def tearDown(self):
+    self.daemonProcess.kill()
+    shutil.rmtree(self.tmpdir)
+    unittest.TestCase.tearDown(self)
+
+  def verifyOut(self, out, expected, failImmediately):
+    m = re.search(expected, out)
+    if failImmediately:
+      self.assertTrue(m != None, "expected to find {0} in '{1}'".format(expected, out))
+           
+    return m != None
+
+  def test_happyPath(self):
+    try:
+      # issue createSnapshot validate that get a snapShot id 
+      cmd = cliRef + createS + [self.tmpdir]
+      r = subprocess.check_output(cmd)
+      self.verifyOut(r, 'snapshot id = [0-9]+', True)
+      m = re.findall(r'[0-9]+', r)
+      sId = m[0]
+      
+      # issue createSnapshot without a path validate get error message and usage prompt 
+      cmd = cliRef + checkoutS + [sId, self.tmpdir]
+      r = subprocess.check_output(cmd).rstrip()
+      self.assertTrue(r == '', "Error: expected '' from checkout, got '{0}'".format(r))
+
+      # run with snapshot id and timeout        
+      cmd = cliRef + runE + ['ls', '--snapshotId={0}'.format(sId), '--timeout={0}'.format('2')]
+      r = subprocess.check_output(cmd).rstrip()
+      self.verifyOut(r, 'run id = [0-9]+', True)
+      r1 = self.get_run_id(r)
+   
+      # run with snapshot id         
+      cmd = cliRef + runE + ['ls', '--snapshotId={0}'.format(sId), ]
+      r = subprocess.check_output(cmd).rstrip()
+      self.verifyOut(r, 'run id = [0-9]+', True)
+      r2 = self.get_run_id(r)
+      
+      pollIds = [r1, r2]
+      # poll with wait and all
+      cmd = cliRef + pollE + pollIds + ['--wait=0', '--all']
+      r = subprocess.check_output(cmd)
+      self.verifyOut(r, 'COMPLETE', True)
+
+      # poll with wait
+      cmd = cliRef + pollE + pollIds + ['--wait=0']
+      r = subprocess.check_output(cmd)
+      self.verifyOut(r, 'COMPLETE', True)
+  
+      # poll with just ids
+      cmd = cliRef + pollE + pollIds 
+      r = subprocess.check_output(cmd)
+      self.verifyOut(r, 'COMPLETE', True)
+    except CalledProcessError as e:
+      self.fail(str(e))
+
+  def get_run_id(self, out):   
+    m = re.findall(r'[0-9]+', out)
+    return m[0]
+       
+       
+  def assert_invalid_command(self, cmd):
+    try:
+      r = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    except Exception as e:
+      return
+    self.fail('{0} did not return an error.'.format(*cmd))
+
+        
+  def test_cmdErrors(self):
+    # issue createSnapshot without a path validate get error message and usage prompt 
+    cmd = cliRef + createS
+    self.assert_invalid_command(cmd)
+      
+    # checkout without snapshot nor dest
+    cmd = cliRef + checkoutS
+    self.assert_invalid_command(cmd)
+  
+    # checkout without dest
+    cmd = cliRef + checkoutS + ['34']
+    self.assert_invalid_command(cmd)
+  
+    # checkout with invalid snapshot
+    cmd = cliRef + checkoutS + ['34', self.tmpdir + "/checkout"]
+    self.assert_invalid_command(cmd)
+     
+    # run with no command
+    cmd = cliRef + runE 
+    self.assert_invalid_command(cmd)
+     
+    # run without snapshot id
+    cmd = cliRef + runE + ['ls']
+    self.assert_invalid_command(cmd)
+     
+    # run with invalid timeout
+    cmd = cliRef + runE + ['ls', '--snapshotId=0', '--timeout=badValue']
+    self.assert_invalid_command(cmd)
+     
+    # poll with no run ids
+    cmd = cliRef + pollE
+    self.assert_invalid_command(cmd)
+        
+        
 
     
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
   
