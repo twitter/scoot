130e08d919d1fdbc68e8c423c51f8019c695aab0
jeanette bruno <jbruno@twitter.com>
2016-12-02 16:55:41 -0500

    removing unittest framework from daemon_cli_test and run_many - integration_py_test seems to be able to run integration_test, so making the other tests more like this one.

--- daemon/protocol/python/tests/daemon_cli_test.py
+++ daemon/protocol/python/tests/daemon_cli_test.py
@@ -5,177 +5,180 @@ import re
 import shutil
 import subprocess
 import tempfile
-import unittest
 
 
 gopath = re.split(":", os.environ['GOPATH'])[0]
 pypath = gopath + '/src/github.com/scootdev/scoot/daemon/protocol/python/scoot'
 cliPath = ['python', pypath + '/scoot.py']
 
-class TestCliCommands(unittest.TestCase):
-  createSReq = ['snapshot', 'create']
-  checkoutSReq = ['snapshot', 'checkout']
-  runEReq = ['exec', 'run']
-  pollEReq = ['exec', 'poll']
-  echoReq = ['echo']
+createSReq = ['snapshot', 'create']
+checkoutSReq = ['snapshot', 'checkout']
+runEReq = ['exec', 'run']
+pollEReq = ['exec', 'poll']
+echoReq = ['echo']
 
-  daemonProcess = None
-  daemonStarted = False;
-     
-  def setUp(self):
-    self.tmpdir = tempfile.mkdtemp()
+daemonProcess = None
+daemonStarted = False;
 
-  def tearDown(self):
-    if tempfile._exists(self.tmpdir):
-      shutil.rmtree(self.tmpdir)
-    unittest.TestCase.tearDown(self)
-
-  def verifyOut(self, stdout, expectedRE, stderr, expectedErr, failImmediately):
-    stdoutOk = True
-    stderrOk = True
-    if expectedRE != '':
-      found = re.search(expectedRE, stdout) >= 0
-    if (expectedRE != '' and not found) or (expectedRE == '' and stdout != ''):
-      stdoutOk = False
-    if (expectedErr != '' and stderr not in expectedErr) or (expectedErr == '' and stderr != ''):
-      stderrOk = False
-    if (not stdoutOk or not stderrOk) and failImmediately: 
-      self.fail("expected match:'{0}' in '{1}' and expected error: '{2}' got '{3}'".format(expectedRE, stdout, expectedErr, stderr))
-           
-    return True
-
-  def test_happy_path(self):
-    global cliPath
-    try:
-      # issue createSnapshot validate that get a snapShot id 
-      cmd = cliPath + self.createSReq + [self.tmpdir]
-      # this test only passes if the validation of create snapshot uses the following code instead of subprocess.check_output()
-      out = self.usePopenForCommand(cmd)
-      self.verifyOut(out[0], 'snapshot id = [0-9]+', out[1], '', True)
-      m = re.findall(r'[0-9]+', out[0])
-      sId = m[0]
-      
-      # issue checkoutSnapshot without a path validate get error message and usage prompt 
-      cmd = cliPath + self.checkoutSReq + [sId, self.tmpdir]
-      r = subprocess.check_output(cmd).rstrip()
-      self.assertTrue(r == '', "Error: expected '' from checkout, got '{0}'".format(r))
-
-      # run with snapshot id and timeout        
-      cmd = cliPath + self.runEReq + ['ls', '--snapshotId={0}'.format(sId), '--timeout={0}'.format('2')]
-      r = subprocess.check_output(cmd).rstrip()
-      self.verifyOut(r, 'run id = [0-9]+',  out[1], '', True)
-      r1 = self.get_run_id(r)
+tmpdir = None
    
-      # run with snapshot id         
-      cmd = cliPath + self.runEReq + ['ls', '--snapshotId={0}'.format(sId), ]
-      r = subprocess.check_output(cmd).rstrip()
-      self.verifyOut(r, 'run id = [0-9]+',  out[1], '', True)
-      r2 = self.get_run_id(r)
+def setUp():
+  global tmpdir
+  tmpdir = tempfile.mkdtemp()
+
+def tearDown():
+  if tempfile._exists(tmpdir):
+    shutil.rmtree(tmpdir)
+
+def verifyOut( stdout, expectedRE, stderr, expectedErr, failImmediately):
+  stdoutOk = True
+  stderrOk = True
+  if expectedRE != '':
+    found = re.search(expectedRE, stdout) >= 0
+  if (expectedRE != '' and not found) or (expectedRE == '' and stdout != ''):
+    stdoutOk = False
+  if (expectedErr != '' and stderr not in expectedErr) or (expectedErr == '' and stderr != ''):
+    stderrOk = False
+  if (not stdoutOk or not stderrOk) and failImmediately: 
+    raise Exception("expected match:'{0}' in '{1}' and expected error: '{2}' got '{3}'".format(expectedRE, stdout, expectedErr, stderr))
+         
+  return True
+
+def test_happy_path():
+  global cliPath
+  try:
+    # issue createSnapshot validate that get a snapShot id 
+    cmd = cliPath + createSReq + [tmpdir]
+    # this test only passes if the validation of create snapshot uses the following code instead of subprocess.check_output()
+    out = usePopenForCommand(cmd)
+    verifyOut(out[0], 'snapshot id = [0-9]+', out[1], '', True)
+    m = re.findall(r'[0-9]+', out[0])
+    sId = m[0]
+    
+    # issue checkoutSnapshot without a path validate get error message and usage prompt 
+    cmd = cliPath + checkoutSReq + [sId, tmpdir]
+    r = subprocess.check_output(cmd).rstrip()
+    if r != '':
+      raise Exception("Error: expected '' from checkout, got '{0}'".format(r))
+
+    # run with snapshot id and timeout        
+    cmd = cliPath + runEReq + ['ls', '--snapshotId={0}'.format(sId), '--timeout={0}'.format('2')]
+    r = subprocess.check_output(cmd).rstrip()
+    verifyOut(r, 'run id = [0-9]+',  out[1], '', True)
+    r1 = get_run_id(r)
+ 
+    # run with snapshot id         
+    cmd = cliPath + runEReq + ['ls', '--snapshotId={0}'.format(sId), ]
+    r = subprocess.check_output(cmd).rstrip()
+    verifyOut(r, 'run id = [0-9]+',  out[1], '', True)
+    r2 = get_run_id(r)
+    
+    pollIds = [r1, r2]
+    # poll with wait and all
+    cmd = cliPath + pollEReq + pollIds + ['--wait=0', '--all']
+    r = subprocess.check_output(cmd)
+    verifyOut(r, 'COMPLETE',  out[1], '', True)
+
+    # poll with wait
+    cmd = cliPath + pollEReq + pollIds + ['--wait=0']
+    r = subprocess.check_output(cmd)
+    verifyOut(r, 'COMPLETE',  out[1], '', True)
+
+    # poll with just ids
+    cmd = cliPath + pollEReq + pollIds 
+    r = subprocess.check_output(cmd)
+    verifyOut(r, 'COMPLETE',  out[1], '', True)
+  except subprocess.CalledProcessError as e:
+    tearDown()
+    raise Exception(str(e))
+
+def get_run_id( out):   
+  m = re.findall(r'[0-9]+', out)
+  return m[0]
+     
+def usePopenForCommand( cmd):
+    outDir = tempfile.mkdtemp()
+    sOut = outDir+"/commandOut.txt"
+    sOutFD = open(sOut, 'w')
+    sErr = outDir+ "/commandErr.txt"
+    sErrFD = open(sErr, 'w')
+    r = subprocess.Popen(cmd, stdout=sOutFD, stderr=sErrFD)
+    r.wait()
+    sOutFDR = open(sOut, 'r')
+    cmdOut = sOutFDR.read()
+    sErrFDR = open(sErr, 'r')
+    cmdErr = sErrFDR.read()
+
+    shutil.rmtree(outDir)
+
+    return [cmdOut, cmdErr]
+     
+def assert_invalid_command( cmd, expectedOut, expectedErr):
+  try:
+    # this test only passes if the validation of create snapshot uses the following code instead of subprocess.check_output()
+    out = usePopenForCommand(cmd)
+    fail = False
+    
+    if (expectedOut != '' and expectedOut not in out[0]) or (expectedOut == '' and out[0] != ''):
+      fail = True
+    if (expectedErr != '' and expectedErr not in out[1]) or (expectedErr == '' and out[1] != ''):
+      fail = True 
+    if fail:
+      raise Exception("cmd:'{0}'\nexpected: '{1}', got '{2}' and expected error: '{3}' got '{4}'".format(cmd, expectedOut, out[0], expectedErr, out[1])) 
+    return
+
+  except subprocess.CalledProcessError as e:
+    raise Exception(str(e))
+
       
-      pollIds = [r1, r2]
-      # poll with wait and all
-      cmd = cliPath + self.pollEReq + pollIds + ['--wait=0', '--all']
-      r = subprocess.check_output(cmd)
-      self.verifyOut(r, 'COMPLETE',  out[1], '', True)
+def test_cmd_errors():
+  global cliPath
 
-      # poll with wait
-      cmd = cliPath + self.pollEReq + pollIds + ['--wait=0']
-      r = subprocess.check_output(cmd)
-      self.verifyOut(r, 'COMPLETE',  out[1], '', True)
-  
-      # poll with just ids
-      cmd = cliPath + self.pollEReq + pollIds 
-      r = subprocess.check_output(cmd)
-      self.verifyOut(r, 'COMPLETE',  out[1], '', True)
-    except subprocess.CalledProcessError as e:
-      self.tearDown()
-      self.fail(str(e))
+  # issue createSnapshot without a path validate get error message and usage prompt 
+  cmd = cliPath + createSReq
+  assert_invalid_command(cmd, '', 'Usage')
+    
+  # checkout without snapshot nor dest
+  cmd = cliPath + checkoutSReq
+  assert_invalid_command(cmd, '', 'Usage')
 
-  def get_run_id(self, out):   
-    m = re.findall(r'[0-9]+', out)
-    return m[0]
-       
-  def usePopenForCommand(self, cmd):
-      outDir = tempfile.mkdtemp()
-      sOut = outDir+"/commandOut.txt"
-      sOutFD = open(sOut, 'w')
-      sErr = outDir+ "/commandErr.txt"
-      sErrFD = open(sErr, 'w')
-      r = subprocess.Popen(cmd, stdout=sOutFD, stderr=sErrFD)
-      r.wait()
-      sOutFDR = open(sOut, 'r')
-      cmdOut = sOutFDR.read()
-      sErrFDR = open(sErr, 'r')
-      cmdErr = sErrFDR.read()
+  # checkout without dest
+  cmd = cliPath + checkoutSReq + ['34']
+  assert_invalid_command(cmd, '', 'Usage')
 
-      shutil.rmtree(outDir)
-
-      return [cmdOut, cmdErr]
-       
-  def assert_invalid_command(self, cmd, expectedOut, expectedErr):
-    try:
-      # this test only passes if the validation of create snapshot uses the following code instead of subprocess.check_output()
-      out = self.usePopenForCommand(cmd)
-      fail = False
+  # checkout with invalid snapshot
+  cmd = cliPath + checkoutSReq + ['344444', tmpdir + "/checkout"]
+  assert_invalid_command(cmd, '', 'No snapshot')
+   
+  # run with no command
+  cmd = cliPath + runEReq 
+  assert_invalid_command(cmd, '', 'Usage')
+   
+  # run without snapshot id
+  cmd = cliPath + runEReq + ['ls']
+  assert_invalid_command(cmd, '', 'Usage')
+   
+  # run with invalid timeout
+  cmd = cliPath + runEReq + ['ls', '--snapshotId=0', '--timeout=badValue']
+  assert_invalid_command(cmd, '', 'Invalid value for timeout')
+   
+  # poll with no run ids
+  cmd = cliPath + pollEReq
+  assert_invalid_command(cmd, '', 'Usage')
       
-      if (expectedOut != '' and expectedOut not in out[0]) or (expectedOut == '' and out[0] != ''):
-        fail = True
-      if (expectedErr != '' and expectedErr not in out[1]) or (expectedErr == '' and out[1] != ''):
-        fail = True 
-      if fail:
-        self.fail("cmd:'{0}'\nexpected: '{1}', got '{2}' and expected error: '{3}' got '{4}'".format(cmd, expectedOut, out[0], expectedErr, out[1])) 
-      return
-
-    except subprocess.CalledProcessError as e:
-      self.fail(str(e))
-#       return
-
-        
-  def test_cmd_errors(self):
-    global cliPath
-  
-    # issue createSnapshot without a path validate get error message and usage prompt 
-    cmd = cliPath + self.createSReq
-    self.assert_invalid_command(cmd, '', 'Usage')
-      
-    # checkout without snapshot nor dest
-    cmd = cliPath + self.checkoutSReq
-    self.assert_invalid_command(cmd, '', 'Usage')
-  
-    # checkout without dest
-    cmd = cliPath + self.checkoutSReq + ['34']
-    self.assert_invalid_command(cmd, '', 'Usage')
-  
-    # checkout with invalid snapshot
-    cmd = cliPath + self.checkoutSReq + ['344444', self.tmpdir + "/checkout"]
-    self.assert_invalid_command(cmd, '', 'No snapshot')
-     
-    # run with no command
-    cmd = cliPath + self.runEReq 
-    self.assert_invalid_command(cmd, '', 'Usage')
-     
-    # run without snapshot id
-    cmd = cliPath + self.runEReq + ['ls']
-    self.assert_invalid_command(cmd, '', 'Usage')
-     
-    # run with invalid timeout
-    cmd = cliPath + self.runEReq + ['ls', '--snapshotId=0', '--timeout=badValue']
-    self.assert_invalid_command(cmd, '', 'Invalid value for timeout')
-     
-    # poll with no run ids
-    cmd = cliPath + self.pollEReq
-    self.assert_invalid_command(cmd, '', 'Usage')
-        
-      # poll with bad wait value
-    cmd = cliPath + self.pollEReq +['1', '--wait=notNumber']
-    self.assert_invalid_command(cmd, '', 'Wait must be an integer')
+    # poll with bad wait value
+  cmd = cliPath + pollEReq +['1', '--wait=notNumber']
+  assert_invalid_command(cmd, '', 'Wait must be an integer')
 
   
 if __name__ == '__main__':
   try:
-    unittest.main()
+    setUp()
+    test_happy_path()
+    test_cmd_errors()
   finally:
     stopReq = ['daemon', 'stop']
     cmd = cliPath + stopReq
     subprocess.call(cmd)
+    tearDown()
     
--- daemon/protocol/python/tests/run_many.py
+++ daemon/protocol/python/tests/run_many.py
@@ -12,172 +12,183 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../scoot'))
 import client_lib as proto
 
 
-class TestManyRunRequests(unittest.TestCase):
-    tmpdir = None
+tmpdir = None
+  
+def setUp():
+  global tmpdir
+  tmpdir = tempfile.mkdtemp()
+
+  proto.start()
+
+  #wait up to 1 second for daemon to start 
+  start = time.time()
+  elapsedTime = 0
+  started = False
+
+  while not started and elapsedTime < 1.0:    
+      try:
+          echo = proto.echo("ping")
+          if echo == "ping":
+              started = True
+      except proto.ScootException as e:
+          elapsedTime = time.time() - start
+  
+  if not started:
+      raise Exception("Connection to daemon couldn't be established in {0} seconds".format(elapsedTime))
+                    
+def tearDown():
+    shutil.rmtree(tmpdir)
+    proto.stop_daemon()
+    
+sleep_ss_key = "sleep_s_id"
+ls_ss_key = "ls_s_id"
+ls_fail_ss_key = "ls_fail_s_id"
+
+# test scenario
+class CmdDef(object):
+    def __init__(self, cmd, snapshot_key, interm_states, final_state):
+        self.cmd = cmd
+        self.snapshot_key = snapshot_key
+        self.interm_states = interm_states
+        self.final_state = final_state
+    
+sleepDef = CmdDef(cmd=["./sleep_script.sh"], 
+                  snapshot_key="sleep_s_id", 
+                  interm_states=[proto.ScootStatus.State.PENDING, 
+                                 proto.ScootStatus.State.PREPARING,
+                                 proto.ScootStatus.State.RUNNING, 
+                                 proto.ScootStatus.State.COMPLETED], 
+                  final_state=proto.ScootStatus.State.COMPLETED)  # run sleep 1, expect complete or running, final status:complete
+lsDef = CmdDef(cmd=["./ls_script.sh"], 
+               snapshot_key="ls_s_id", 
+               interm_states=[proto.ScootStatus.State.PREPARING, 
+                              proto.ScootStatus.State.PENDING, 
+                              proto.ScootStatus.State.RUNNING, 
+                              proto.ScootStatus.State.COMPLETED], 
+               final_state=proto.ScootStatus.State.COMPLETED)  # run ls script 5 times expect complete, running or queued, final status:complete
+failDef = CmdDef(cmd=["./ls_script.sh"], 
+                 snapshot_key="ls_fail_s_id", 
+                 interm_states=[proto.ScootStatus.State.PENDING,
+                                proto.ScootStatus.State.PREPARING, 
+                                proto.ScootStatus.State.RUNNING, 
+                                proto.ScootStatus.State.COMPLETED], 
+                 final_state=proto.ScootStatus.State.COMPLETED)  # run ls script 5 times expect complete, running or queued, final status:failed
+
+def test_many():
+  global sleepDef, lsDef, failDef
+  rpc_timeout_ns = int(1000 * 1e6)
+  
+  runs = {}
+  
+  # Make the snapshots.
+  ss_ids = make_snapshots()
+
+  # Run the sleep script
+  id = proto.run(argv=sleepDef.cmd, timeout_ns=rpc_timeout_ns, snapshot_id=ss_ids[sleepDef.snapshot_key])
+  runs[id] = "sleep"
+  
+  # run ~200 requests, 10 repetitions of 20 successful, 1 error
+  for i in range(5):
+      for j in range(10):
+          try:
+              id = proto.run(argv=lsDef.cmd, timeout_ns=rpc_timeout_ns, snapshot_id=ss_ids[lsDef.snapshot_key])
+              runs[id] = "ls"
+          except proto.ScootException as e:
+              if "Runner is busy" not in str(e):
+                  raise Exception("Run error, not resource limitation.{}".format(str(e)))
       
-    def setUp(self):
-        self.tmpdir = tempfile.mkdtemp()
-
-        proto.start()
-
-        #wait up to 1 second for daemon to start 
-        start = time.time()
-        elapsedTime = 0
-        started = False
-
-        while not started and elapsedTime < 1.0:    
-            try:
-                echo = proto.echo("ping")
-                if echo == "ping":
-                    started = True
-            except proto.ScootException as e:
-                elapsedTime = time.time() - start
-        
-        if not started:
-            self.fail("Connection to daemon couldn't be established in {0} seconds".format(elapsedTime))
-                        
-    def tearDown(self):
-        shutil.rmtree(self.tmpdir)
-        proto.stop_daemon()
-        unittest.TestCase.tearDown(self)
-        
-    sleep_ss_key = "sleep_s_id"
-    ls_ss_key = "ls_s_id"
-    ls_fail_ss_key = "ls_fail_s_id"
-    
-    # test scenario
-    class CmdDef(object):
-        def __init__(self, cmd, snapshot_key, interm_states, final_state):
-            self.cmd = cmd
-            self.snapshot_key = snapshot_key
-            self.interm_states = interm_states
-            self.final_state = final_state
-        
-    sleepDef = CmdDef(cmd=["./sleep_script.sh"], 
-                      snapshot_key="sleep_s_id", 
-                      interm_states=[proto.ScootStatus.State.PENDING, 
-                                     proto.ScootStatus.State.PREPARING,
-                                     proto.ScootStatus.State.RUNNING, 
-                                     proto.ScootStatus.State.COMPLETED], 
-                      final_state=proto.ScootStatus.State.COMPLETED)  # run sleep 1, expect complete or running, final status:complete
-    lsDef = CmdDef(cmd=["./ls_script.sh"], 
-                   snapshot_key="ls_s_id", 
-                   interm_states=[proto.ScootStatus.State.PREPARING, 
-                                  proto.ScootStatus.State.PENDING, 
-                                  proto.ScootStatus.State.RUNNING, 
-                                  proto.ScootStatus.State.COMPLETED], 
-                   final_state=proto.ScootStatus.State.COMPLETED)  # run ls script 5 times expect complete, running or queued, final status:complete
-    failDef = CmdDef(cmd=["./ls_script.sh"], 
-                     snapshot_key="ls_fail_s_id", 
-                     interm_states=[proto.ScootStatus.State.PENDING,
-                                    proto.ScootStatus.State.PREPARING, 
-                                    proto.ScootStatus.State.RUNNING, 
-                                    proto.ScootStatus.State.COMPLETED], 
-                     final_state=proto.ScootStatus.State.COMPLETED)  # run ls script 5 times expect complete, running or queued, final status:failed
-
-    def test_many(self):
-        rpc_timeout_ns = int(1000 * 1e6)
-        
-        runs = {}
-        
-        # Make the snapshots.
-        ss_ids = self.make_snapshots()
-    
-        # Run the sleep script
-        id = proto.run(argv=self.sleepDef.cmd, timeout_ns=rpc_timeout_ns, snapshot_id=ss_ids[self.sleepDef.snapshot_key])
-        runs[id] = "sleep"
-        
-        # run ~200 requests, 10 repetitions of 20 successful, 1 error
-        for i in range(5):
-            for j in range(10):
-                try:
-                    id = proto.run(argv=self.lsDef.cmd, timeout_ns=rpc_timeout_ns, snapshot_id=ss_ids[self.lsDef.snapshot_key])
-                    runs[id] = "ls"
-                except proto.ScootException as e:
-                    if "Runner is busy" not in str(e):
-                        raise Exception("Run error, not resource limitation.{}".format(str(e)))
-            
-            try:
-                id = proto.run(argv=self.failDef.cmd, timeout_ns=rpc_timeout_ns, snapshot_id=ss_ids[self.failDef.snapshot_key])
-            except proto.ScootException as e:
-                if "Runner is busy" not in str(e):
-                    raise Exception("Run error, not resource limitation.{}".format(str(e)))
-                
-            runs[id] = "fail"
-    
-                
-        # get all statuses
-        statuses = proto.poll(run_ids=runs.keys(), timeout_ns=0, return_all=True)
-        self.assertIntermediateStatuses(statuses, runs)
-        
-        # wait for all runs to finish and validate their status
-        start = time.time()
-        allDone = False
-        elapsedTime = 0
-        while not allDone and elapsedTime < 4.0:
-            ids = runs.keys()
-            statuses = proto.poll(run_ids=ids, timeout_ns=int(3000 * 1e6), return_all=False)
-            if len(statuses) == len(ids):
-                allDone = True
-            elapsedTime = time.time() - start
-            
-        self.assertTrue(len(ids) == len(statuses), "runs did not finish.")
-        self.assertCompleteStatuses(statuses, runs)
-            
-    
-    # make the following snapshots
-    # sleep snapshot: has a "sleep_script.sh" that sleeps for 0.5 seconds
-    # ls snapshot: has an "ls_script.sh" that issues a successful ls command
-    # ls fail snapshot: has an "ls_script.sh" that issues a failing ls command
-    # will return a dict of the snapshot ids
-    def make_snapshots(self):
-        # make the files that will go into the snapshots        
-        sleep_script = os.path.join(self.tmpdir, "sleep_script.sh")
-        open(sleep_script, 'w').write("#!/bin/sh\nsleep 0.5")
-        os.chmod(sleep_script, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
-    
-        ls_script = os.path.join(self.tmpdir, "ls_script.sh")
-        open(ls_script, 'w').write("#!/bin/sh\nls resource.txt")
-        os.chmod(ls_script, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
-    
-        resource = os.path.join(self.tmpdir, "resource.txt")
-        open(resource, 'w').write("content")
-    
-        # make the snapshots
-        sleep_ss_id = proto.create_snapshot(sleep_script)
-        ls_ss_id = proto.create_snapshot(os.path.join(self.tmpdir, "*"))
-        fail_ls_ss_id = proto.create_snapshot(ls_script)
-        
-        # return a dict of the snapshot ids
-        return {self.sleep_ss_key:sleep_ss_id, self.ls_ss_key:ls_ss_id, self.ls_fail_ss_key:fail_ls_ss_id}
-    
-        
-    def assertIntermediateStatuses(self, statuses, runs):
-        for status in statuses:
-            state = status.state
-            if runs[status.run_id] == "sleep":
-                self.assertTrue(state in self.sleepDef.interm_states, "run type:{0}, expected on of {1}, got {2}".format("sleep", self.sleepDef.interm_states, state))
-            if runs[status.run_id] == "ls":
-                self.assertTrue(state in self.lsDef.interm_states, "run type:{0}, expected on of {1}, got {2}".format("ls", self.lsDef.interm_states, state))
-            if runs[status.run_id] == "fail":
-                self.assertTrue(state in self.failDef.interm_states, "run type:{0}, expected on of {1}, got {2}".format("fail", self.failDef.interm_states, state))
-            
-        return True
-    
-    def assertCompleteStatuses(self, statuses, runs):
-        for status in statuses:
-            state = status.state
-            if runs[status.run_id] == "sleep":
-                self.assertTrue(state == self.sleepDef.final_state, "run type:{0}, expected {1}, got {2}".format("sleep", self.sleepDef.final_state, state))
-            if runs[status.run_id] == "ls":
-                self.assertTrue(state == self.lsDef.final_state, "run type:{0}, expected {1}, got {2}".format("ls", self.lsDef.final_state, state))
-            if runs[status.run_id] != "fail":
-                self.assertTrue(state == self.failDef.final_state, "run type:{0}, expected {1}, got {2}".format("fail", self.failDef.final_state, state))
+      try:
+          id = proto.run(argv=failDef.cmd, timeout_ns=rpc_timeout_ns, snapshot_id=ss_ids[failDef.snapshot_key])
+      except proto.ScootException as e:
+          if "Runner is busy" not in str(e):
+              raise Exception("Run error, not resource limitation.{}".format(str(e)))
           
-        return True
+      runs[id] = "fail"
+
+          
+  # get all statuses
+  statuses = proto.poll(run_ids=runs.keys(), timeout_ns=0, return_all=True)
+  assertIntermediateStatuses(statuses, runs)
+  
+  # wait for all runs to finish and validate their status
+  start = time.time()
+  allDone = False
+  elapsedTime = 0
+  while not allDone and elapsedTime < 4.0:
+      ids = runs.keys()
+      statuses = proto.poll(run_ids=ids, timeout_ns=int(3000 * 1e6), return_all=False)
+      if len(statuses) == len(ids):
+          allDone = True
+      elapsedTime = time.time() - start
+      
+  if len(ids) != len(statuses):
+    raise Exception("runs did not finish.")
+  assertCompleteStatuses(statuses, runs)
+        
+
+# make the following snapshots
+# sleep snapshot: has a "sleep_script.sh" that sleeps for 0.5 seconds
+# ls snapshot: has an "ls_script.sh" that issues a successful ls command
+# ls fail snapshot: has an "ls_script.sh" that issues a failing ls command
+# will return a dict of the snapshot ids
+def make_snapshots():
+  # make the files that will go into the snapshots        
+  sleep_script = os.path.join(tmpdir, "sleep_script.sh")
+  open(sleep_script, 'w').write("#!/bin/sh\nsleep 0.5")
+  os.chmod(sleep_script, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+  ls_script = os.path.join(tmpdir, "ls_script.sh")
+  open(ls_script, 'w').write("#!/bin/sh\nls resource.txt")
+  os.chmod(ls_script, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+
+  resource = os.path.join(tmpdir, "resource.txt")
+  open(resource, 'w').write("content")
+
+  # make the snapshots
+  sleep_ss_id = proto.create_snapshot(sleep_script)
+  ls_ss_id = proto.create_snapshot(os.path.join(tmpdir, "*"))
+  fail_ls_ss_id = proto.create_snapshot(ls_script)
+  
+  # return a dict of the snapshot ids
+  return {sleep_ss_key:sleep_ss_id, ls_ss_key:ls_ss_id, ls_fail_ss_key:fail_ls_ss_id}
+
+    
+def assertIntermediateStatuses( statuses, runs):
+  for status in statuses:
+      state = status.state
+      if runs[status.run_id] == "sleep":
+          if not state in sleepDef.interm_states:
+            raise Exception("run type:{0}, expected on of {1}, got {2}".format("sleep", sleepDef.interm_states, state))
+      if runs[status.run_id] == "ls":
+          if not state in lsDef.interm_states:
+            raise Exception("run type:{0}, expected on of {1}, got {2}".format("ls", lsDef.interm_states, state))
+      if runs[status.run_id] == "fail":
+          if not state in failDef.interm_states:
+            raise Exception("run type:{0}, expected on of {1}, got {2}".format("fail", failDef.interm_states, state))
+      
+  return True
+
+def assertCompleteStatuses( statuses, runs):
+    for status in statuses:
+        state = status.state
+        if runs[status.run_id] == "sleep":
+            if state != sleepDef.final_state:
+              raise Exception("run type:{0}, expected {1}, got {2}".format("sleep", sleepDef.final_state, state))
+        if runs[status.run_id] == "ls":
+            if state != lsDef.final_state:
+              raise Exception("run type:{0}, expected {1}, got {2}".format("ls", lsDef.final_state, state))
+        if runs[status.run_id] != "fail":
+            if state != failDef.final_state:
+              raise Exception("run type:{0}, expected {1}, got {2}".format("fail", failDef.final_state, state))
+      
+    return True
     
         
 
 if __name__ == '__main__':
-    unittest.main()
+  try:
+    setUp()
+    test_many()
+  finally:
+    tearDown()
   
