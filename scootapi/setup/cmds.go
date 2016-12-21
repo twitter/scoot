87649860bed355dc0375d773ffbf24dfb8df36c6
Dan Bentley <dbentley@twitter.com>
2016-12-21 00:20:53 -0500

    scootapi/setup: improve waiting and killing

--- scootapi/setup/cmds.go
+++ scootapi/setup/cmds.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/scootdev/scoot/os/temp"
 )
@@ -27,6 +28,9 @@ type Cmds struct {
 	watching []*exec.Cmd
 	mu       sync.Mutex
 	tmp      *temp.TempDir
+
+	wg     sync.WaitGroup
+	killed bool
 }
 
 // Create a new Cmds
@@ -44,7 +48,7 @@ func NewSignalHandlingCmds(tmp *temp.TempDir) *Cmds {
 	go func() {
 		var sig os.Signal
 		sig = <-sigchan
-		log.Printf("signal %s received, shutting down", sig)
+		log.Printf("signal %s received; shutting down", sig)
 		r.Kill()
 		os.Exit(1)
 	}()
@@ -59,12 +63,61 @@ func (c *Cmds) Kill() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	for _, cmd := range c.watching {
-		if p := cmd.Process; p != nil {
-			log.Printf("Killing: %v\n", cmd.Path)
-			// TODO(dbentley): send a SIGTERM and wait and then send a SIGKILL only if it hasn't finished later
-			p.Kill()
+	// If we already tried to kill, then don't try to kill again. And don't try to kill
+	// in the future.
+	if c.killed {
+		return
+	}
+	c.killed = true
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		for sig := range sigchan {
+			log.Printf("signal %s received; ignoring as we are already killing all", sig)
 		}
+	}()
+
+	log.Printf("Killing %d cmds", len(c.watching))
+
+	// Wait for all to be done.
+	allDoneCh := make(chan struct{})
+	go func() {
+		c.wg.Wait()
+		close(allDoneCh)
+	}()
+
+	// Sending a signal to -PGID sends it to all processes in the proces sgroup.
+	pgid := -1 * syscall.Getpgrp()
+
+	if len(c.watching) == 0 {
+		return
+	}
+
+	// Send SIGINT, then send SIGKILL 5 seconds later if any are still running
+	log.Printf("SIGINT: process group %d", pgid)
+	if err := syscall.Kill(pgid, syscall.SIGINT); err != nil {
+		panic(err)
+	}
+
+	// Unlock so that remove can do its job (and signal alldoneCh)
+	c.mu.Unlock()
+	select {
+	case <-allDoneCh:
+		log.Printf("All completed")
+	case <-time.After(5 * time.Second):
+		log.Printf("Still waiting")
+	}
+	c.mu.Lock()
+
+	if len(c.watching) == 0 {
+		return
+	}
+
+	log.Printf("SIGKILL: process group%d", pgid)
+	if err := syscall.Kill(pgid, syscall.SIGKILL); err != nil {
+		panic(err)
 	}
 }
 
@@ -75,6 +128,7 @@ func (c *Cmds) Command(path string, arg ...string) *exec.Cmd {
 	cmd := exec.Command(path, arg...)
 	// TODO(dbentley): redirect output to files in c.tmp so we don't deluge stdout/stderr
 	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+	c.wg.Add(1)
 	c.watching = append(c.watching, cmd)
 	return cmd
 }
@@ -87,7 +141,8 @@ func (c *Cmds) StartCmd(cmd *exec.Cmd) error {
 	if err == nil {
 		go func() {
 			cmd.Wait()
-			log.Printf("Cmd %v finished; killing", cmd.Path)
+			log.Printf("Cmd %v finished", cmd.Path)
+			c.remove(cmd)
 			c.Kill()
 		}()
 	}
@@ -118,6 +173,7 @@ func (c *Cmds) Run(path string, arg ...string) error {
 func (c *Cmds) remove(cmd *exec.Cmd) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
+	c.wg.Done()
 	for i, other := range c.watching {
 		if other == cmd {
 			c.watching = append(c.watching[0:i], c.watching[i+1:]...)
--- scootapi/setup/ports.go
+++ scootapi/setup/ports.go
@@ -7,11 +7,18 @@ import (
 	"time"
 )
 
-// WaitForPort waits ~5 seconds for a process to listen to the port, and returns an error if
+// WaitForPort waits 10 seconds for a process to listen to the port, and returns an error if
 // the port remains open
 func WaitForPort(port string) error {
-	log.Printf("Waiting for port %v", port)
-	for i := 0; i < 10; i++ {
+	return WaitForPortTimeout(port, 10*time.Second)
+}
+
+// WaitForPortTimeout waits timeout for a process to listen to the port, and returns an error if
+// the port remains open
+func WaitForPortTimeout(port string, timeout time.Duration) error {
+	log.Printf("Waiting for port %v for %v", port, timeout)
+	end := time.Now().Add(timeout)
+	for !time.Now().After(end) {
 		// Use exec.Command because we don't worry about these getting orphaned,
 		// and don't want to fill up our Cmds's list of running cmds
 		cmd := exec.Command("nc", "-z", "localhost", port)
