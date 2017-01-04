e10c7f830358f258916ca4dc2fffd8fe778bede8
Dan Bentley <dbentley@twitter.com>
2017-01-04 12:33:55 -0500

    review responses

--- snapshot/db.go
+++ snapshot/db.go
@@ -4,7 +4,7 @@ import (
 	"github.com/scootdev/scoot/snapshot/git/repo"
 )
 
-// ID identifies a value in DB. Opaque to the client.
+// ID identifies a Value in DB. (Cf. doc.go for explanation of Value) Opaque to the client.
 type ID string
 
 // DB is the Scoot Database, allowing creation, distribution, and export of Values.
@@ -12,34 +12,35 @@ type DB interface {
 	// Ingest
 
 	// IngestDir ingests a directory directly.
-	// The created value is a Snapshot.
+	// The created Value is a Snapshot.
 	IngestDir(dir string) (ID, error)
 
 	// IngestGitCommit ingests the commit identified by commitish from ingestRepo
 	// commitish may be any string that identifies a commit
-	// The created value is a SnapshotWithHistory.
+	// The created Value is a SnapshotWithHistory.
 	IngestGitCommit(ingestRepo *repo.Repository, commitish string) (ID, error)
 
 	// Operations
 
 	// UnwrapSnapshotHistory unwraps a SnapshotWithHistory and returns a Snapshot ID.
 	// Errors if id does not identify a SnapshotWithHistory.
-	SnapshotForRevision(id ID) (ID, error)
+	UnwrapSnapshotHistory(id ID) (ID, error)
 
 	// Distribute
 
-	// Upload makes sure the value id is uploaded, returning an ID that can be used
+	// Upload makes sure the Value id is uploaded, returning an ID that can be used
 	// anywhere or an error
 	Upload(id ID) (ID, error)
 
-	// Download makes sure the value id is downloaded, returning an ID that can be used
+	// Download makes sure the Value id is downloaded, returning an ID that can be used
 	// on this computer or an error
-	Download(id SnapID) (SnapID, error)
+	Download(id ID) (ID, error)
 
 	// Export
 
-	// Checkout puts the value identified by id in the local filesystem, returning
+	// Checkout puts the Value identified by id in the local filesystem, returning
 	// the path where it lives or an error.
+	// TODO(dbentley): should this download if necessary?
 	Checkout(id ID) (path string, err error)
 
 	// ReleaseCheckout releases a path from a previous Checkout. This allows Scoot to reuse
