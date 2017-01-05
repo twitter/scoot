008b9fb0a57e364e249ce6482dd3a9e259719392
Dan Bentley <dbentley@twitter.com>
2017-01-05 10:57:27 -0500

    stuff

--- sched/scheduler/stateful_scheduler_property_test.go
+++ sched/scheduler/stateful_scheduler_property_test.go
@@ -18,11 +18,8 @@ func Test_StatefulScheduler_TasksDistributedEvenly(t *testing.T) {
 	taskMap := make(map[string]cluster.NodeId)
 
 	/*jobId, _ :=*/ s.ScheduleJob(jobDef)
-	s.step()
 
-	for len(s.inProgressJobs) > 0 {
-		s.step()
-
+	for s.step(); len(s.inProgressJobs) > 0; s.step() {
 		for nodeId, state := range s.clusterState.nodes {
 			if state.runningTask != noTask {
 				taskMap[state.runningTask] = nodeId
@@ -38,9 +35,12 @@ func Test_StatefulScheduler_TasksDistributedEvenly(t *testing.T) {
 	// The in memory workers aren't doing anything interesting except sleeping distribution
 	// should be even with in 180 - 220 nodes otherwise something is wrong.
 	for nodeId, taskCount := range taskCountMap {
-		if taskCount < 180 || taskCount > 220 {
+		// TODO(dbentley): I see an error in Travis where:
+		// TaskCountMap: map[node2:198 node1:209 node4:199 node3:166 node5:205]
+		// This is odd, because they only add up to 977 instead of 100, so 23 are being lost altogether.
+		if taskCount < 180 || taskCount > 220 || true {
 			t.Fatalf(`Tasks were not evenly distributed across nodes.  Expected each node
-				to have 190 to 210 tasks executed on it. %v had an unequal number of tasks %v scheduled 
+				to have 180 (150 b/c of flakiness! TODO(dbentley)) to 220 tasks executed on it. %v had an unequal number of tasks %v scheduled
 				on it.  TaskCountMap: %+v`, nodeId, taskCount, taskCountMap)
 		}
 	}
