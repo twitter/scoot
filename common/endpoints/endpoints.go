e6ef8c7232cb87973391edb1dba9b6ebaecfc72f
kmccaffrey <kmccaffrey@twitter.com>
2017-01-17 16:12:38 -0800

    get stats from sidecar

--- common/endpoints/endpoints.go
+++ common/endpoints/endpoints.go
@@ -4,10 +4,13 @@ package endpoints
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/scootdev/scoot/common/stats"
@@ -15,19 +18,29 @@ import (
 
 // Returns an http handler at 'addr' which can retrieved with 'uri' if specified. Also serves static files from tmpDir if specified.
 func NewTwitterServer(addr string, stats stats.StatsReceiver, handlers map[string]http.Handler) *TwitterServer {
+	return NewTwitterServerWithSidecar(addr, stats, handlers, nil)
+}
+
+func NewTwitterServerWithSidecar(
+	addr string,
+	stats stats.StatsReceiver,
+	handlers map[string]http.Handler,
+	sidecarStatPorts []int) *TwitterServer {
 	return &TwitterServer{
-		Addr:     addr,
-		Stats:    stats,
-		Handlers: handlers,
+		Addr:             addr,
+		Stats:            stats,
+		Handlers:         handlers,
+		SidecarStatPorts: sidecarStatPorts,
 	}
 }
 
 // A stats receiver that provides HTTP access for metric scraping with
 // Twitter-style endpoints.
 type TwitterServer struct {
-	Addr     string
-	Stats    stats.StatsReceiver
-	Handlers map[string]http.Handler
+	Addr             string
+	Stats            stats.StatsReceiver
+	Handlers         map[string]http.Handler
+	SidecarStatPorts []int
 }
 
 func (s *TwitterServer) Serve() error {
@@ -60,9 +73,56 @@ func (s *TwitterServer) statsHandler(w http.ResponseWriter, r *http.Request) {
 	const contentTypeVal = "application/json; charset=utf-8"
 	w.Header().Set(contentTypeHdr, contentTypeVal)
 
+	// get stats from this Twitter Server server
+	serviceStats := s.Stats.Render(false)
+
+	var allStats map[string]float64
+	json.Unmarshal(serviceStats, &allStats)
+
+	// get stats for the sidecars
+	sidecarStatsCh := make(chan []byte, len(s.SidecarStatPorts))
+	var wg sync.WaitGroup
+	for _, p := range s.SidecarStatPorts {
+		wg.Add(1)
+		go func(port int) {
+			defer wg.Done()
+			// sidecars must be running on the same machine and also be twitter servers
+			rsp, err := http.Get(fmt.Sprintf("http://localhost:%v/admin/metrics.json", port))
+			if err == nil {
+				defer rsp.Body.Close()
+				body, err := ioutil.ReadAll(rsp.Body)
+
+				if err == nil {
+					sidecarStatsCh <- body
+
+				} else {
+					log.Printf("ERROR: Reading Body of Stats Response for Sidecar at Port %v, Error: %v", port, err)
+				}
+			} else {
+				log.Printf("ERROR: Getting Stats for Sidecar at Port %v, returned an error %v", port, err)
+			}
+		}(p)
+	}
+
+	// wait until we have all the stats then combine
+	wg.Wait()
+	close(sidecarStatsCh)
+	for statsRsp := range sidecarStatsCh {
+		json.Unmarshal(statsRsp, &allStats)
+	}
+
+	// format stats and return
+	var statsAsBinary []byte
+	var err error
 	pretty := r.URL.Query().Get("pretty") == "true"
-	str := s.Stats.Render(pretty)
-	if _, err := io.Copy(w, bytes.NewBuffer(str)); err != nil {
+
+	if pretty {
+		statsAsBinary, err = json.MarshalIndent(allStats, "", "  ")
+	} else {
+		statsAsBinary, err = json.Marshal(allStats)
+	}
+
+	if _, err = io.Copy(w, bytes.NewBuffer(statsAsBinary)); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
