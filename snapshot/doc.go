2c6dca77e160673951dd6727028e42ca048c6291
Dan Bentley <dbentley@twitter.com>
2017-01-20 17:10:16 -0500

    review resposnes

--- snapshot/doc.go
+++ snapshot/doc.go
@@ -7,6 +7,8 @@ An FSSnapshot is a snapshot of filesystem state (like a tar file).
 
 A GitCommitSnapshot mirrors a git commit: a Snapshot, commit metadata, and an optional list of parent GitCommitSnapshots.
 
+Snapshots can be used as the input to a Task, and we will store the output of a Task as a Snapshot.
+
 There may be more Snapshot kinds in the future. E.g., a file (without the directory structure).
 */
 package snapshot
