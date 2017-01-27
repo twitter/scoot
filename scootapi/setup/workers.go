ca81fb831ea661a31360560b5f639437411d649d
Dan Bentley <dbentley@twitter.com>
2017-01-27 14:39:27 -0500

    fix build

--- scootapi/setup/workers.go
+++ scootapi/setup/workers.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/scootdev/scoot/os/temp"
+	"github.com/scootdev/scoot/scootapi"
 	"github.com/scootdev/scoot/snapshot"
 	"github.com/scootdev/scoot/snapshot/bundlestore"
 	"github.com/scootdev/scoot/snapshot/git/gitdb"
