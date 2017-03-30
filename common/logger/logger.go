d59ebeb39511681069d25ee46d4e1cc73b05d38e
rcouto <rcouto@twitter.com>
2017-03-30 14:19:44 -0400

    comments and add sethandler / gethandler / new

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -1,19 +1,46 @@
 // Wrapper for github.com/inconshreveable/log15
-// Allows configurable handlers for users of scoot
+// Allows configurable handlers for users of Scoot logging
 // log15 handler docs are available at:
 // https://godoc.org/github.com/inconshreveable/log15#Handler
+//
+// Ex. For logging file & line # to stdout,
+//
+// import (
+// 		log "github.com/scootdev/scoot/common/logger"
+// 		"github.com/inconshreveable/log15"
+// )
+//
+// h := log15.CallerFileHandler(log15.StdoutHandler)
+// log.SetHandler(h)
+
 package logger
 
 import log "github.com/inconshreveable/log15"
 
 var Log = log.New()
 
-func Info(msg string, ctx ...interface{}) {
-	Log.Info(msg, ctx)
+// New returns a new Logger that has this logger's context plus the given context
+func New(ctx ...interface{}) log.Logger {
+	return log.New(ctx)
 }
 
+// GetHandler gets the handler associated with the logger.
+func GetHandler() log.Handler {
+	return Log.GetHandler()
+}
+
+// SetHandler updates the logger to write records to the specified handler.
+func SetHandler(h log.Handler) {
+	Log.SetHandler(h)
+}
+
+// Log a message at the given level with context key/value pairs
 func Debug(msg string, ctx ...interface{}) {
 	Log.Debug(msg, ctx)
+}
+
+func Info(msg string, ctx ...interface{}) {
+	Log.Info(msg, ctx)
 }
 
 func Crit(msg string, ctx ...interface{}) {
