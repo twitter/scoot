20962b0bd8b397c198811c78bf2bafc0a7ccce2f
Jason Schiller <jschiller@twitter.com>
2017-09-21 15:48:44 -0700

    Simplified deps.sh just enough to get it working again on OSX.

--- deps.sh
+++ deps.sh
@@ -2,14 +2,15 @@
 # Add new direct and transitive dependencies as submodules.
 #
 echo "Note: caller must validate/curate/commit changes to .gitmodules and 'vendor/'"
-echo "FIXME: will not work for gopkg.in: 'Unknown SSL protocol error in connection to gopkg.in:-9838'"
+echo "FIXME: may not work for gopkg.in on OSX: 'Unknown SSL protocol error in connection to gopkg.in:-9838'"
+echo "       see https://github.com/niemeyer/gopkg/issues/49 for context"
+echo '       also try temporarily doing:  git config --global url."http://".insteadOf https://  '
 
 set -euo pipefail
 trap "exit" INT TERM
 trap 'rm -rf ${GOPATH_NEW}' EXIT
 
 SCOOT_ORIG="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-GOPATH_ORIG="${GOPATH}"
 GOPATH_NEW="$(mktemp -d -t TEMP.XXXXXXX)"
 export GOPATH="${GOPATH_NEW}"
 
@@ -18,7 +19,6 @@ get_deps() {
     cd "${GOPATH}/src/$1"
     for need in $(go list -f '{{join .Deps "\n"}}' ./... | \
                   xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}' 2>&1 | \
-                  grep -v "gopkg.in" | \
                   grep "can't load package" | \
                   sed -E 's,[^"]*"([^"]*).*,\1,' | \
                   grep '\..*/'); do
@@ -27,26 +27,20 @@ get_deps() {
     done
 }
 
-if [[ -z "${DEP_REPO:-}" ]]; then
-    DEP_REPO="github.com/scootdev/scoot"
-    scootdev="${GOPATH}/src/$(dirname ${DEP_REPO})"
-    mkdir -p "${scootdev}" && cd "${scootdev}"
-    git clone "https://${DEP_REPO}"
-else
-    depdir="${GOPATH}/src/$(dirname ${DEP_REPO})"
-    mkdir -p "${depdir}" && cd "${depdir}"
-    cp -r "${GOPATH_ORIG}/src/${DEP_REPO}" ./
-fi
+depRepo="github.com/twitter/scoot"
+depDir="${GOPATH}/src/$(dirname ${depRepo})"
+mkdir -p "${depDir}" && cd "${depDir}"
+cp -R "${SCOOT_ORIG}" "./$(basename ${depRepo})"
 
 echo "Darwin Deps."
 export GOOS=darwin GOARCH=amd64
-get_deps "${DEP_REPO}"
+get_deps "${depRepo}"
 
 echo "Windows Deps."
 export GOOS=windows GOARCH=amd64
-get_deps "${DEP_REPO}"
+get_deps "${depRepo}"
 
-HANDLED=$(find ${GOPATH} -name .git | sort | uniq | sed -E "s,${GOPATH}/src/|/\.git,,g" | grep -v scootdev)
+HANDLED=$(find ${GOPATH} -name .git | sort | uniq | sed -E "s,${GOPATH}/src/|/\.git,,g" | grep -v twitter)
 
 cd "${SCOOT_ORIG}"
 for dep in ${HANDLED}; do
