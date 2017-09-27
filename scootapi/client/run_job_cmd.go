3e9dc49d3ffaffcc9536be1d0074a9c6bb3b303f
rcouto <rcouto@twitter.com>
2017-09-27 13:04:09 -0400

    Add back taskID

--- scootapi/client/run_job_cmd.go
+++ scootapi/client/run_job_cmd.go
@@ -104,6 +104,7 @@ func (c *runJobCmd) run(cl *simpleCLIClient, cmd *cobra.Command, args []string) 
 			taskDef.Command = scoot.NewCommand()
 			taskDef.Command.Argv = jt.Args
 			taskDef.SnapshotId = &jt.SnapshotID
+			taskDef.TaskId = &jt.TaskID
 			jobDef.Tasks = append(jobDef.Tasks, taskDef)
 			if jt.TimeoutMs > 0 {
 				taskDef.TimeoutMs = &jt.TimeoutMs
