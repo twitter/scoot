e7241ff0e24941e506f599f365a6a8f0a53c802c
Drew Gassaway <dgassaway@twitter.com>
2017-09-28 14:09:03 -0400

    fix compilation failure

--- remote/server/server.go
+++ remote/server/server.go
@@ -4,11 +4,13 @@
 package server
 
 import (
+	"context"
+
 	"github.com/twitter/scoot/remote/protocol"
 )
 
 // Implements protocol ExecutionServer interface
 func Execute(context.Context, *protocol.RequestPlaceholder) (*protocol.ResponsePlaceholder, error) {
 	rp := protocol.ResponsePlaceholder{Output: "execute!!!"}
-	return &rp
+	return &rp, nil
 }
