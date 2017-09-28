a978c17f961464d8fc515baa5fa0bb095e66b15e
rcouto <rcouto@twitter.com>
2017-09-28 15:11:37 -0400

    Revert non-panicing block

--- runner/runners/queue.go
+++ runner/runners/queue.go
@@ -222,13 +222,13 @@ func (c *QueueController) Abort(run runner.RunID) (runner.RunStatus, error) {
 func (c *QueueController) abort(run runner.RunID) (runner.RunStatus, error) {
 	if run == c.runningID {
 		if c.runningAbort != nil {
-			fields := log.Fields{"currentRun": c.runningID}
-			if c.runningCmd != nil {
-				fields["jobID"] = c.runningCmd.JobID
-				fields["taskID"] = c.runningCmd.TaskID
-				fields["tag"] = c.runningCmd.Tag
-			}
-			log.WithFields(fields).Info("Aborting")
+			log.WithFields(
+				log.Fields{
+					"currentRun": c.runningID,
+					"jobID":      c.runningCmd.JobID,
+					"taskID":     c.runningCmd.TaskID,
+					"tag":        c.runningCmd.Tag,
+				}).Info("Aborting")
 			close(c.runningAbort)
 			c.runningAbort = nil
 		}
