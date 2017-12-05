41e966959d1baa7ab5231d6420486fdf9ea0e4ff
rcouto <rcouto@twitter.com>
2017-12-05 17:54:51 -0500

    Add --local_store_path

--- snapshot/bazel/filer.go
+++ snapshot/bazel/filer.go
@@ -13,13 +13,29 @@ func MakeDefaultBzFiler() *bzFiler {
 	}
 }
 
+func MakeBzFilerWithLocalStore(localStorePath string) *bzFiler {
+	return &bzFiler{
+		command:        "fs_util",
+		updater:        snapshots.MakeNoopUpdater(),
+		localStorePath: localStorePath,
+	}
+}
+
 // Satisfies snapshot.Checkouter, snapshot.Ingester, and snapshot.Updater
 type bzFiler struct {
-	command string
-	updater snapshot.Updater
+	command        string
+	updater        snapshot.Updater
+	localStorePath string
+	// Not yet implemented:
+	// bypassLocalStore bool
+	// skipServer       bool
+	// serverAddress    string
 }
 
 func (bf *bzFiler) RunCmd(args []string) ([]byte, error) {
+	if bf.localStorePath != "" {
+		args = append(args, "--local_store_path", bf.localStorePath)
+	}
 	cmd := exec.Command(bf.command, args...)
 	return cmd.Output()
 }
