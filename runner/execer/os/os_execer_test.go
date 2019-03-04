f862f10877df01f27ea1e618e0f266b2ed62ed39
rcouto <rcouto@twitter.com>
2019-03-04 15:43:25 -0500

    clean up

--- runner/execer/os/os_execer_test.go
+++ runner/execer/os/os_execer_test.go
@@ -4,9 +4,17 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/twitter/scoot/common/log/hooks"
 	"github.com/twitter/scoot/runner/execer"
-	// log "github.com/sirupsen/logrus"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	log.AddHook(hooks.NewContextHook())
+	logrusLevel, _ := log.ParseLevel("debug")
+	log.SetLevel(logrusLevel)
+}
 
 func NewBoundedTestExecer(memCap execer.Memory, pg procGetter) *osExecer {
 	return &osExecer{memCap: memCap, pg: pg}
@@ -14,60 +22,60 @@ func NewBoundedTestExecer(memCap execer.Memory, pg procGetter) *osExecer {
 
 // Tests that single process memory usage is counted
 func TestOsExecerMemUsage(t *testing.T) {
-	limit := 10
-	pg := &testProcGetter{procs: []string{fmt.Sprintf("1 1 1 %d", limit)}}
-	e := NewBoundedTestExecer(execer.Memory(limit*bytesToKB), pg)
+	rss := 10
+	pg := &testProcGetter{procs: []string{fmt.Sprintf("1 1 1 %d", rss)}}
+	e := NewBoundedTestExecer(execer.Memory(0), pg)
 	mem, err := e.memUsage(1)
-	if mem != execer.Memory(limit*bytesToKB) || err != nil {
+	if mem != execer.Memory(rss*bytesToKB) || err != nil {
 		t.Fatalf("%v: %v mem", err, mem)
 	}
 }
 
 // Tests that memory of processes spawned by a process in original process's process group are counted
 func TestParentProcGroup(t *testing.T) {
-	limit := 10
-	pg := &testProcGetter{procs: []string{fmt.Sprintf("1 1 1 %d", limit), fmt.Sprintf("2 1 1 %d", limit), fmt.Sprintf("3 2 2 %d", limit)}}
-	e := NewBoundedTestExecer(execer.Memory(limit*bytesToKB), pg)
+	rss := 10
+	pg := &testProcGetter{procs: []string{fmt.Sprintf("1 1 1 %d", rss), fmt.Sprintf("2 1 1 %d", rss), fmt.Sprintf("3 2 2 %d", rss)}}
+	e := NewBoundedTestExecer(execer.Memory(0), pg)
 	mem, err := e.memUsage(1)
-	if mem != execer.Memory(limit*3*bytesToKB) || err != nil {
+	if mem != execer.Memory(rss*3*bytesToKB) || err != nil {
 		t.Fatalf("%v: %v mem", err, mem)
 	}
 }
 
 // Tests that memory of processes within process group are counted
 func TestProcGroup(t *testing.T) {
-	limit := 10
-	pg := &testProcGetter{procs: []string{fmt.Sprintf("1 1 1 %d", limit), fmt.Sprintf("2 1 1 %d", limit), fmt.Sprintf("3 1 2 %d", limit)}}
-	e := NewBoundedTestExecer(execer.Memory(limit*bytesToKB), pg)
+	rss := 10
+	pg := &testProcGetter{procs: []string{fmt.Sprintf("1 1 1 %d", rss), fmt.Sprintf("2 1 1 %d", rss), fmt.Sprintf("3 1 2 %d", rss)}}
+	e := NewBoundedTestExecer(execer.Memory(0), pg)
 	mem, err := e.memUsage(1)
-	if mem != execer.Memory(limit*3*bytesToKB) || err != nil {
+	if mem != execer.Memory(rss*3*bytesToKB) || err != nil {
 		t.Fatalf("%v: %v mem", err, mem)
 	}
 }
 
 // Tests that memory of unrelated processes are not counted
 func TestUnrelatedProcs(t *testing.T) {
-	limit := 10
+	rss := 10
 	pg := &testProcGetter{procs: []string{
-		fmt.Sprintf("1 1 1 %d", limit), fmt.Sprintf("2 1 1 %d", limit), fmt.Sprintf("3 1 2 %d", limit), fmt.Sprintf("100 100 100 100")}}
-	e := NewBoundedTestExecer(execer.Memory(limit*bytesToKB), pg)
+		fmt.Sprintf("1 1 1 %d", rss), fmt.Sprintf("2 1 1 %d", rss), fmt.Sprintf("3 1 2 %d", rss), fmt.Sprintf("100 100 100 100")}}
+	e := NewBoundedTestExecer(execer.Memory(0), pg)
 	mem, err := e.memUsage(1)
-	if mem != execer.Memory(limit*3*bytesToKB) || err != nil {
+	if mem != execer.Memory(rss*3*bytesToKB) || err != nil {
 		t.Fatalf("%v: %v mem", err, mem)
 	}
 }
 
 // Tests that processes related through original process's pgid and their children are counted
 func TestParentProcGroupAndChildren(t *testing.T) {
-	limit := 10
+	rss := 10
 	pg := &testProcGetter{procs: []string{
-		fmt.Sprintf("0 0 0 %d", limit), fmt.Sprintf("1 0 1 %d", limit),
-		fmt.Sprintf("2 1 1 %d", limit), fmt.Sprintf("3 1 2 %d", limit),
-		fmt.Sprintf("100 0 0 %d", limit), fmt.Sprintf("101 100 100 %d", limit),
-		fmt.Sprintf("1000 1000 1000 %d", limit)}}
-	e := NewBoundedTestExecer(execer.Memory(limit*bytesToKB), pg)
+		fmt.Sprintf("0 0 0 %d", rss), fmt.Sprintf("1 0 1 %d", rss),
+		fmt.Sprintf("2 1 1 %d", rss), fmt.Sprintf("3 1 2 %d", rss),
+		fmt.Sprintf("100 0 0 %d", rss), fmt.Sprintf("101 100 100 %d", rss),
+		fmt.Sprintf("1000 1000 1000 %d", rss)}}
+	e := NewBoundedTestExecer(execer.Memory(0), pg)
 	mem, err := e.memUsage(1)
-	if mem != execer.Memory(limit*6*bytesToKB) || err != nil {
+	if mem != execer.Memory(rss*6*bytesToKB) || err != nil {
 		t.Fatalf("%v: %v mem", err, mem)
 	}
 }
@@ -80,6 +88,5 @@ type testProcGetter struct {
 func (pg *testProcGetter) getProcs() (
 	allProcesses map[int]proc, relatedProcesses map[int]proc, processGroups map[int][]proc,
 	parentProcesses map[int][]proc, err error) {
-	// log.Info("k")
 	return pg.parseProcs(pg.procs)
 }
