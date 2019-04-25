939888ed8c065d962c7fad6872d640e9858e8d22
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2019-04-25 00:29:54 -0700

    add debug logs for ./get_fs_util.sh and bump lockfile

--- get_fs_util.sh
+++ get_fs_util.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 # Pulls in fs_util tool for use in remote exec ingestion, checkout, and CAS ops
-set -e
+set -euxo pipefail
 
 pants_release="1.13.0rc0+4e02e65b"
 pants_release_url=$(echo $pants_release | sed 's/+/%2B/')
 
 get_fs_util() {
-	case "$(uname -s)" in
+        case "$(uname -s)" in
 
-		Darwin)
-			local url="https://binaries.pantsbuild.org/bin/fs_util/mac/10.11/$pants_release_url/fs_util"
-			echo "Fetching Darwin from $url"
-			;;
+                Darwin)
+                        local url="https://binaries.pantsbuild.org/bin/fs_util/mac/10.11/$pants_release_url/fs_util"
+                        echo "Fetching Darwin from $url"
+                        ;;
 
-		Linux)
-			local url="https://binaries.pantsbuild.org/bin/fs_util/linux/x86_64/$pants_release_url/fs_util"
-			echo "Fetching Linux from $url"
-			;;
+                Linux)
+                        local url="https://binaries.pantsbuild.org/bin/fs_util/linux/x86_64/$pants_release_url/fs_util"
+                        echo "Fetching Linux from $url"
+                        ;;
 
-		*)
-			echo "Expected Darwin or Linux output from uname, received $(uname -s)"
-			;;
-	esac
+                *)
+                        echo "Expected Darwin or Linux output from uname, received $(uname -s)"
+                        ;;
+        esac
 
-	firstgopath=${GOPATH%%:*}
+        firstgopath=${GOPATH%%:*}
 
     if [[ -f "$firstgopath/bin/fs_util-$pants_release" ]]; then
         echo "Target file exists locally"
         return
     fi
-
-	wget "$url" -O "$firstgopath/bin/fs_util-$pants_release"
-	chmod +x "$firstgopath/bin/fs_util-$pants_release"
+        mkdir -p "$firstgopath/bin"
+        wget "$url" -O "$firstgopath/bin/fs_util-$pants_release"
+        chmod +x "$firstgopath/bin/fs_util-$pants_release"
     ln -s -f "$firstgopath/bin/fs_util-$pants_release" "$firstgopath/bin/fs_util"
 }
 
