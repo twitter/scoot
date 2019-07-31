3cc337ac6d5acb22677c79f98e4b8a055f6b0f7d
rcouto <rcouto@twitter.com>
2019-07-31 14:51:28 -0400

    Cancel integration test

--- binaries/bazel-integration/main.go
+++ binaries/bazel-integration/main.go
@@ -17,6 +17,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/genproto/googleapis/longrunning"
+	google_rpc_code "google.golang.org/genproto/googleapis/rpc/code"
 
 	"github.com/twitter/scoot/bazel"
 	"github.com/twitter/scoot/bazel/remoteexecution"
@@ -24,6 +25,7 @@ import (
 	"github.com/twitter/scoot/common/log/hooks"
 	"github.com/twitter/scoot/os/temp"
 	"github.com/twitter/scoot/scootapi"
+	"github.com/twitter/scoot/scootapi/setup"
 	"github.com/twitter/scoot/tests/testhelpers"
 )
 
@@ -61,17 +63,110 @@ func main() {
 	// TODO: WaitForClusterToBeReady should wait for CAS/ApiServers too
 	time.Sleep(3 * time.Second)
 
+	testSuccessfulCommand(gopath, clusterCmds)
+	testCancelledCommand(gopath, clusterCmds)
+
+	clusterCmds.Kill()
+}
+
+func installBinaries() error {
+	testhelpers.InstallBinary("bzutil")
+	b, err := exec.Command("sh", "get_fs_util.sh").CombinedOutput()
+	if err != nil {
+		log.Error(string(b))
+	}
+	return err
+}
+
+func testSuccessfulCommand(gopath string, clusterCmds *setup.Cmds) {
+	expectedCommandDigest := remoteexecution.Digest{
+		Hash:      "1b00e10d51c107c1a1f06ebdc09dea3c45e06fd257481d085d4e37566f6a6041",
+		SizeBytes: 76,
+	}
+	expectedActionDigest := remoteexecution.Digest{
+		Hash:      "776f8cae4d90c0719121d4131ea18df38f88e20794e3907bed69195ef986a72f",
+		SizeBytes: 138,
+	}
+	op := testRunCmd(gopath, clusterCmds, 1, expectedCommandDigest, expectedActionDigest)
+	// Get Operation
+	time.Sleep(3 * time.Second)
+	b, err := getOperation(gopath, op.GetName())
+	if err != nil {
+		testhelpers.KillAndExit1(clusterCmds, err)
+	}
+	json.Unmarshal(b, op)
+	if !op.GetDone() {
+		testhelpers.KillAndExit1(clusterCmds, fmt.Errorf("Expected operation to be Done. Op: %v", op))
+	}
+	// TODO: propagate op.Result through json Unmarshaling and verify op.Response is set per contract defined at
+	// https://github.com/googleapis/go-genproto/blob/c506a9f9061087022822e8da603a52fc387115a8/googleapis/longrunning/operations.pb.go#L50
+	// if op.GetResponse() == nil {
+	// 	testhelpers.KillAndExit1(clusterCmds, fmt.Errorf("Expected result to be set. Op: %v", op))
+	// }
+	log.Info("Operation completed successfully")
+}
+
+func testCancelledCommand(gopath string, clusterCmds *setup.Cmds) {
+	expectedCommandDigest := remoteexecution.Digest{
+		Hash:      "0392024bf028c9fd456824d64aacd8937679c451ff512c7a43da72680bf532fd",
+		SizeBytes: 78,
+	}
+	expectedActionDigest := remoteexecution.Digest{
+		Hash:      "19ca48544f8ad500bf07b0ed06ec0fbe32f9474325d381a81a1acd23ccee52af",
+		SizeBytes: 138,
+	}
+	op := testRunCmd(gopath, clusterCmds, 100, expectedCommandDigest, expectedActionDigest)
+
+	// Get Operation
+	b, err := getOperation(gopath, op.GetName())
+	if err != nil {
+		testhelpers.KillAndExit1(clusterCmds, err)
+	}
+	json.Unmarshal(b, op)
+	if op.GetDone() {
+		testhelpers.KillAndExit1(clusterCmds, fmt.Errorf("Expected operation to not be Done. Op: %v", op))
+	}
+
+	// Cancel Operation
+	_, err = cancelOperation(gopath, op.GetName())
+	if err != nil {
+		testhelpers.KillAndExit1(clusterCmds, fmt.Errorf("Unable to cancel operation: %s", err))
+	}
+
+	// Get Operation and verify it was cancelled
+	time.Sleep(3 * time.Second)
+	b, err = getOperation(gopath, op.GetName())
+	log.Errorf("b: %s", string(b))
+	if err != nil {
+		testhelpers.KillAndExit1(clusterCmds, err)
+	}
+	json.Unmarshal(b, op)
+	if !op.GetDone() {
+		testhelpers.KillAndExit1(clusterCmds, fmt.Errorf("Expected operation to be Done. Op: %v", op))
+	}
+	// TODO: propagate op.Result through json Unmarshaling and verify op.Error is set per contract defined at
+	// https://github.com/googleapis/go-genproto/blob/c506a9f9061087022822e8da603a52fc387115a8/googleapis/longrunning/operations.pb.go#L50
+	// if op.GetError() == nil {
+	// 	testhelpers.KillAndExit1(clusterCmds, fmt.Errorf("Expected result to be of type Operation_Error, was %+v", op.GetResult()))
+	// }
+	// if op.GetError().GetCode() != int32(google_rpc_code.Code_CANCELLED) {
+	// 	testhelpers.KillAndExit1(clusterCmds, fmt.Errorf("op.Error.Code to be %d, was %d", google_rpc_code.Code_CANCELLED, op.GetError().GetCode()))
+	// }
+	// if op.GetError().GetMessage() != "CANCELLED" {
+	// 	testhelpers.KillAndExit1(clusterCmds, fmt.Errorf("op.Error.Message to be 'CANCELLED', was %s", op.GetError().GetMessage()))
+	// }
+
+	log.Info("Operation cancelled successfully")
+}
+
+func testRunCmd(gopath string, clusterCmds *setup.Cmds, timeToSleep int, expectedCommandDigest, expectedActionDigest remoteexecution.Digest) *longrunning.Operation {
 	// Upload Command
-	b, err := uploadCommand(gopath)
+	b, err := uploadCommand(gopath, timeToSleep)
 	if err != nil {
 		testhelpers.KillAndExit1(clusterCmds, err)
 	}
 	commandDigest := &remoteexecution.Digest{}
 	json.Unmarshal(b, commandDigest)
-	expectedCommandDigest := remoteexecution.Digest{
-		Hash:      "1b00e10d51c107c1a1f06ebdc09dea3c45e06fd257481d085d4e37566f6a6041",
-		SizeBytes: 76,
-	}
 
 	if err = assertEqual(*commandDigest, expectedCommandDigest); err != nil {
 		testhelpers.KillAndExit1(clusterCmds, err)
@@ -104,10 +199,6 @@ func main() {
 	}
 	actionDigest := &remoteexecution.Digest{}
 	json.Unmarshal(b, actionDigest)
-	expectedActionDigest := remoteexecution.Digest{
-		Hash:      "776f8cae4d90c0719121d4131ea18df38f88e20794e3907bed69195ef986a72f",
-		SizeBytes: 138,
-	}
 	if err = assertEqual(*actionDigest, expectedActionDigest); err != nil {
 		testhelpers.KillAndExit1(clusterCmds, err)
 	}
@@ -119,30 +210,11 @@ func main() {
 	operation := &longrunning.Operation{}
 	json.Unmarshal(b, operation)
 	log.Infof("Operation executing: %v", operation)
-	// Get Operation
-	time.Sleep(3 * time.Second)
-	b, err = getOperation(gopath, operation.GetName())
-	if err != nil {
-		testhelpers.KillAndExit1(clusterCmds, err)
-	}
-	json.Unmarshal(b, operation)
-	if !operation.GetDone() {
-		testhelpers.KillAndExit1(clusterCmds, fmt.Errorf("Expected operation to be Done. Op: %v", operation))
-	}
-	clusterCmds.Kill()
+	return operation
 }
 
-func installBinaries() error {
-	testhelpers.InstallBinary("bzutil")
-	b, err := exec.Command("sh", "get_fs_util.sh").CombinedOutput()
-	if err != nil {
-		log.Error(string(b))
-	}
-	return err
-}
-
-func uploadCommand(gopath string) ([]byte, error) {
-	return exec.Command(gopath+"/bin/bzutil", "upload_command", "--json", "--cas_addr=localhost:12100", "--output_files=/output/f1", "--output_dirs=/output/d1,/output/subdir/d2", "--platform_props=JDK_SYMLINK=.jvm", "sleep", "1").Output()
+func uploadCommand(gopath string, timeToSleep int) ([]byte, error) {
+	return exec.Command(gopath+"/bin/bzutil", "upload_command", "--json", "--cas_addr=localhost:12100", "--output_files=/output/f1", "--output_dirs=/output/d1,/output/subdir/d2", "--platform_props=JDK_SYMLINK=.jvm", "sleep", fmt.Sprintf("%d", timeToSleep)).Output()
 }
 
 func saveDirectory(gopath string) ([]byte, error) {
@@ -167,6 +239,10 @@ func execute(gopath, actionDigest string) ([]byte, error) {
 
 func getOperation(gopath, name string) ([]byte, error) {
 	return exec.Command(gopath+"/bin/bzutil", "get_operation", "--json", fmt.Sprintf("--name=%s", name)).Output()
+}
+
+func cancelOperation(gopath, name string) ([]byte, error) {
+	return exec.Command(gopath+"/bin/bzutil", "cancel_operation", "--json", fmt.Sprintf("--name=%s", name)).Output()
 }
 
 func assertEqual(recvd, expected remoteexecution.Digest) error {
