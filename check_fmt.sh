a1d6023967c3abeeddf5c4a9fe804e6d50aa22d7
Drew Gassaway <dgassaway@twitter.com>
2019-10-16 12:13:03 -0700

    quotes in check_fmt

--- check_fmt.sh
+++ check_fmt.sh
@@ -4,7 +4,7 @@ set -e
 check_fmt() {
     local fmt
     fmt=$(gofmt -l -s .)
-    if [[ ! -z $fmt ]]; then
+    if [[ ! -z "$fmt" ]]; then
         echo "failed gofmt:"
         echo "$fmt"
         exit 1
