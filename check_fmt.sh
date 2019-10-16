cc0c443906d07d3eab285db6c4e3cdb1e574a923
Drew Gassaway <dgassaway@twitter.com>
2019-10-16 12:04:00 -0700

    actually enforce gofmt in travis

---
+++ check_fmt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+check_fmt() {
+    local fmt
+    fmt=$(gofmt -l -s .)
+    if [[ ! -z $fmt ]]; then
+        echo "failed gofmt:"
+        echo "$fmt"
+        exit 1
+    fi
+}
+
+check_fmt
