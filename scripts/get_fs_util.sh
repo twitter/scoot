cf11b3b88cc7c52020765ca9b0a0e40ca1d26b8b
Anthony Pratti <apratti@twitter.com>
2020-11-30 16:08:32 -0500

    Fix permission issue

--- scripts/get_fs_util.sh
+++ scripts/get_fs_util.sh
@@ -28,8 +28,8 @@ get_fs_util() {
         echo "Target file exists locally"
         return
     fi
-
-	wget "$url" -O "$firstgopath/bin/fs_util-$pants_release"
+  # Github actions need this to run as su
+	sudo wget "$url" -O "$firstgopath/bin/fs_util-$pants_release"
 	chmod +x "$firstgopath/bin/fs_util-$pants_release"
     ln -s -f "$firstgopath/bin/fs_util-$pants_release" "$firstgopath/bin/fs_util"
 }
