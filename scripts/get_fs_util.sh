a714212af77e4c3ba8b896883d9f264e159eb08d
Anthony Pratti <apratti@twitter.com>
2020-12-02 03:43:59 -0500

    Remove formating in untouched file

--- scripts/get_fs_util.sh
+++ scripts/get_fs_util.sh
@@ -28,9 +28,10 @@ get_fs_util() {
         echo "Target file exists locally"
         return
     fi
+
 	wget "$url" -O "$firstgopath/bin/fs_util-$pants_release"
 	chmod +x "$firstgopath/bin/fs_util-$pants_release"
-	ln -s -f "$firstgopath/bin/fs_util-$pants_release" "$firstgopath/bin/fs_util"
+    ln -s -f "$firstgopath/bin/fs_util-$pants_release" "$firstgopath/bin/fs_util"
 }
 
 mkdir -p "$firstgopath/bin"
