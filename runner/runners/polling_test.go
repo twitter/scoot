c0c65b233b29d11d82ab85433e47850dae3b985e
cheyennem <cheyennem@twitter.com>
2021-09-07 09:17:14 -0700

    Remove flaky test

--- runner/runners/polling_test.go
+++ runner/runners/polling_test.go
@@ -2,11 +2,9 @@ package runners
 
 import (
 	"fmt"
-	"sort"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/twitter/scoot/common/stats"
 	"github.com/twitter/scoot/runner"
 	"github.com/twitter/scoot/runner/execer/execers"
@@ -147,77 +145,4 @@ func TestPolling_ErrorPolling(t *testing.T) {
 	// Now let it finish
 	ex.Resume()
 
-}
-
-func TestPollingWorker_Intervalse(t *testing.T) {
-	_, _, poller := setupPoller()
-
-	st, err := poller.Run(&runner.Command{Argv: []string{"complete 42"}})
-	if err != nil {
-		t.Fatal(st, err)
-	}
-	st, _, err = runner.FinalStatus(poller, st.RunID)
-	if err != nil {
-		t.Fatal(st, err)
-	}
-	if st.State != runner.COMPLETE || st.ExitCode != 42 {
-		t.Fatal(st, err)
-	}
-}
-
-func setupTestPollerIntervals(period time.Duration) (*execers.SimExecer, runner.StatusQueryNower, runner.Service) {
-	ex := execers.NewSimExecer()
-	filerMap := runner.MakeRunTypeMap()
-	filerMap[runner.RunTypeScoot] = snapshot.FilerAndInitDoneCh{Filer: snapshots.MakeInvalidFiler(), IDC: nil}
-	single := NewSingleRunner(ex, filerMap, NewNullOutputCreator(), nil, stats.NopDirsMonitor, runner.EmptyID)
-	chaos := NewChaosRunner(single)
-	var nower runner.StatusQueryNower = &instrumentedNower{ChaosRunner: chaos}
-	poller := NewPollingService(chaos, nower, period)
-	return ex, nower, poller
-}
-
-type instrumentedNower struct {
-	*ChaosRunner
-	queryNowFreq FreqList
-	lastQueryNow time.Time
-}
-
-type FreqList []time.Duration
-
-func (f FreqList) Len() int           { return len(f) }
-func (f FreqList) Swap(i, j int)      { f[i], f[j] = f[j], f[i] }
-func (f FreqList) Less(i, j int) bool { return f[i] <= f[j] }
-
-func (in *instrumentedNower) QueryNow(q runner.Query) ([]runner.RunStatus, runner.ServiceStatus, error) {
-	if in.queryNowFreq == nil {
-		in.queryNowFreq = []time.Duration{}
-	} else {
-		in.queryNowFreq = append(in.queryNowFreq, time.Since(in.lastQueryNow))
-	}
-	in.lastQueryNow = time.Now()
-	return in.ChaosRunner.QueryNow(q)
-}
-
-// Test poller period increases
-func TestPollingWorker_QueryIntervalIncrease(t *testing.T) {
-	_, nower, poller := setupTestPollerIntervals(10 * time.Microsecond)
-	st, err := poller.Run(&runner.Command{Argv: []string{"complete 43"}})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Sleep for long enough to poll multiple times
-	time.Sleep(time.Duration(100) * time.Microsecond)
-
-	st, _, err = runner.FinalStatus(poller, st.RunID)
-	if err != nil || st.State != runner.COMPLETE || st.ExitCode != 43 {
-		t.Fatal(st, err)
-	}
-
-	inNower, ok := nower.(*instrumentedNower)
-	assert.True(t, ok)
-	assert.True(t, len(inNower.queryNowFreq) > 0)
-
-	// Check for increasing duration
-	assert.True(t, sort.IsSorted(inNower.queryNowFreq))
 }
