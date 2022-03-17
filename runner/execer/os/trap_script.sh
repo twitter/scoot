7b24803a5573ed0d644bf58de74f0529c6c6f50a
Drew Gassaway <dgassaway@twitter.com>
2022-03-17 15:53:51 -0500

    python -> python3 in tests

--- runner/execer/os/trap_script.sh
+++ runner/execer/os/trap_script.sh
@@ -3,5 +3,5 @@ trap ':' SIGTERM
 
 while :
 do
-	python -c 'import time; exec("x=[]\nfor i in range(1):\n x.append(\" \" * 1024*1024)\n time.sleep(.1)")'
+	python3 -c 'import time; exec("x=[]\nfor i in range(1):\n x.append(\" \" * 1024*1024)\n time.sleep(.1)")'
 done
